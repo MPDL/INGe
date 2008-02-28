fc610ff5c3ecb65cc7ea41ac96a5672bba4af491
tschraut <tschraut@localhost>
2008-02-28 09:04:59 +0000

    constructor

--- pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/util/PubFileVOPresentation.java
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/util/PubFileVOPresentation.java
@@ -1,44 +1,86 @@
 package de.mpg.escidoc.pubman.util;
 
-import java.io.IOException;
-import java.io.InputStream;
+import javax.faces.component.UIComponent;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.context.FacesContext;
 
-import org.apache.myfaces.trinidad.model.UploadedFile;
-
+import de.mpg.escidoc.pubman.EditItemPage;
+import de.mpg.escidoc.pubman.appbase.FacesBean;
+import de.mpg.escidoc.pubman.editItem.EditItem;
+import de.mpg.escidoc.pubman.editItem.EditItemSessionBean;
 import de.mpg.escidoc.services.common.valueobjects.PubFileVO;
 
-public class PubFileVOPresentation implements UploadedFile {
+public class PubFileVOPresentation extends FacesBean {
 
+	private int index;
 	private PubFileVO file;
+	private HtmlCommandButton removeButton = new HtmlCommandButton();
 	
-	public PubFileVOPresentation(PubFileVO file)
+	public PubFileVOPresentation()
 	{
-		this.file = file; 
+		this.file = new PubFileVO();
+	}
+	
+	public PubFileVOPresentation(int fileIndex, PubFileVO file)
+	{
+		this.index = fileIndex; 
+		this.file = file;
+		this.removeButton.setTitle("btnRemove_" + fileIndex);
 	}
 
-	public void dispose() {
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public PubFileVO getFile() {
+		return file;
+	}
+
+	public void setFile(PubFileVO file) {
+		this.file = file;
+	}
+
+	public HtmlCommandButton getRemoveButton() {
+		return removeButton;
+	}
+
+	public void setRemoveButton(HtmlCommandButton removeButton) {
+		this.removeButton = removeButton;
+	}
+	
+	public String removeFile ()
+	{
+ 		EditItem editItem = (EditItem)getSessionBean(EditItem.class);
+ 		EditItemSessionBean editItemSessionBean = (EditItemSessionBean)getSessionBean(EditItemSessionBean.class);
+		if(index < editItem.getFiles().size())
+		{
+			editItem.getPubItem().getFiles().remove(index);
+			editItemSessionBean.getFiles().remove(index);
+			
+			// ensure that at least one upload component is visible
+			if(editItem.getPubItem().getFiles().size() == 0)
+			{
+				editItem.getPubItem().getFiles().add(new PubFileVO());
+			}
+			if(editItem.getFiles().size() == 0)
+			{
+				editItem.getFiles().add(new PubFileVOPresentation());
+			}
+			// clear the view root
+			UIComponent component = FacesContext.getCurrentInstance().getViewRoot().findComponent("EditItem:fileUploads");
+			if (component != null)
+	        {
+	        	component.getParent().getChildren().remove(component);
+	        }
+			//return "loadEditItemIntermediate";
+		}
+		editItem.reorganizeFileIndexes();
+		//editItem.init();
+		return "loadEditItem";
 		
 	}
-
-	public String getFilename() {
-		return file.getName();
-	}
-
-	public InputStream getInputStream() throws IOException {
-		return null;
-	}
-
-	public long getLength() {
-		return file.getSize();
-	}
-
-	public Object getOpaqueData() {
-		return null;
-	}
-
-	public String getContentType()
-	{
-		return file.getContentTypeString();
-	}
-
 }
