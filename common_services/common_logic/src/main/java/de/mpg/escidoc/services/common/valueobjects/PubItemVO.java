1da3042993503cf256a25c289490d4744b3772e9
Michael Franke <franke@mpdl.mpg.de>
2008-04-08 14:59:06 +0000

    Renaming and enabled tests

---
+++ common_services/common_logic/src/main/java/de/mpg/escidoc/services/common/valueobjects/PubItemVO.java
@@ -0,0 +1,49 @@
+package de.mpg.escidoc.services.common.valueobjects;
+
+import org.apache.log4j.Logger;
+
+public class PubItemVO extends ItemVO {
+	
+	/**
+	 * Default constructor.
+	 */
+	public PubItemVO()
+	{
+		
+	}
+	
+	/**
+	 * Clone constructor.
+	 * 
+	 * @param itemVO The item to be copied.
+	 */
+	public PubItemVO(PubItemVO itemVO)
+	{
+		super(itemVO);
+	}
+	
+	
+	public MdsPublicationVO getMetadata()
+	{
+		if (getMetadataSets() != null && getMetadataSets().size() > 0 && getMetadataSets().get(0) instanceof MdsPublicationVO)
+		{
+			return (MdsPublicationVO) getMetadataSets().get(0);
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public void setMetadata(MdsPublicationVO mdsPublicationVO)
+	{
+		if (getMetadataSets().size() > 0 && getMetadataSets().get(0) instanceof MdsPublicationVO)
+		{
+			getMetadataSets().set(0, mdsPublicationVO);
+		}
+		else if (getMetadataSets() != null)
+		{
+			getMetadataSets().add(mdsPublicationVO);
+		}
+	}
+}
