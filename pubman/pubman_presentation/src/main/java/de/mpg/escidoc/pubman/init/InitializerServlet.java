76efcece116e28c30ddfaf794678821081f36660
Michael Franke <franke@mpdl.mpg.de>
2008-04-08 09:31:39 +0000

    Init servlet for statistics

---
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/init/InitializerServlet.java
@@ -0,0 +1,14 @@
+package de.mpg.escidoc.pubman.init;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class InitializerServlet extends HttpServlet {
+
+	@Override
+	public void init() throws ServletException {
+
+		
+	}
+
+}
