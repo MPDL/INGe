ea8f2e6427e0d5e3c05b1de436fbb7ae2d6c0f1b
tendres <tendres@localhost>
2008-08-20 14:36:44 +0000

    import new files

---
+++ common_services/search_ear/src/test/java/de/mpg/escidoc/services/test/search/SearchTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package de.mpg.escidoc.services.test.search;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Integration test suite for service "Search".
+ * @author endres
+ *
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+//					 BracketsTest.class
+//                     ,ConstantsTest.class
+//                     ,ExpressionsTest.class                    
+//                     ,OperationsTest.class
+//                     ,PhrasesTest.class
+//                     ,QueryParserTest.class                    
+//                     ,SearchPubItemsByAffiliationTest.class
+                     SimpleSearchTest.class
+//                     ,AdvancedSearchTest.class
+//                     ,SearchAndOutputTest.class
+                    })
+public class SearchTest {
+
+}
