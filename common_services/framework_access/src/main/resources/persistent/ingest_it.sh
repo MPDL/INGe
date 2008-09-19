a0d820d32c54221b1d064735689c9d20bb735694
bartosch <bartosch@localhost>
2008-09-19 11:27:56 +0000

    little correction in ingest_it.sh - one parameter less

--- common_services/framework_access/src/main/resources/persistent/ingest_it.sh
+++ common_services/framework_access/src/main/resources/persistent/ingest_it.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 FEDORA_USER="fedoraAdmin"
 FEDORA_PASSWORD="fedoraAdmin"
-${FEDORA_HOME}/client/bin/fedora-ingest.sh  d . info:fedora/fedora-system:FOXML-1.1 o localhost:8082 ${FEDORA_USER} ${FEDORA_PASSWORD} http
+${FEDORA_HOME}/client/bin/fedora-ingest.sh  d . info:fedora/fedora-system:FOXML-1.1 localhost:8082 ${FEDORA_USER} ${FEDORA_PASSWORD} http ""
