3a0344ab5a97ee53c7a1774b610386fec420625f
meichau <meichau@localhost>
2008-12-09 10:31:36 +0000

    added line to multiple author paste

--- pubman/pubman_presentation/src/main/webapp/resources/eSciDoc_JavaScript/eSciDoc_component_JavaScript/eSciDoc_item_list.js
+++ pubman/pubman_presentation/src/main/webapp/resources/eSciDoc_JavaScript/eSciDoc_component_JavaScript/eSciDoc_item_list.js
@@ -29,17 +29,81 @@
 */
 
 function addItemListFunctions() {
-	$('.itemList').each(function(i,ele){$(ele).find('.mediumView').each(function(j,elem){$(elem).hide();}); $(ele).find('.collapse').each(function(j,elem){$(elem).hide();}); $(ele).find('.expand').each(function(j,elem){$(elem).show();});})
+	$('.itemList').each(
+			function(i,ele){
+				$(ele).find('.mediumView').each(
+						function(j,elem){
+							$(elem).hide();
+						});
+				$(ele).find('.collapse').each(
+						function(j,elem){
+							$(elem).hide();
+						});
+				$(ele).find('.expand').each(
+						function(j,elem){
+							$(elem).show();
+						});
+			})
+
 	$('.checkboxSelectButton').click(function(){$(this).siblings('.selectMenu').show();});
 	$('.checkBoxCloseSelectButton').click(function(){$(this).parent().hide();});
 		$('.selectMenu').find('.selectTotal').click(function(){$(this).parents('.itemList').find('.itemCheckBox').each(function(i, elem){elem.checked=true;}); $(this).parents('.selectMenu').hide();});
 		$('.selectMenu').find('.selectAll').click(function(){$(this).parents('.itemList').find('.itemCheckBox').each(function(i, elem){elem.checked=true;}); $(this).parents('.selectMenu').hide();});
 		$('.selectMenu').find('.selectNone').click(function(){$(this).parents('.itemList').find('.itemCheckBox').each(function(i, elem){elem.checked=false;});});
 		$('.selectMenu').find('a').each(function(i,elem){$(elem).click(function(){$(this).parents('.selectMenu').hide()});});
-	$('.headerSwitchView').find('.expand').click(function(){$(this).hide(); $(this).siblings('.collapse').show(); $(this).parents('.itemList').find('.listItem').find('.expand:visible').each(function(i, elem){$(elem).trigger('click');});});
-	$('.headerSwitchView').find('.collapse').click(function(){$(this).hide(); $(this).siblings('.expand').show(); $(this).parents('.itemList').find('.listItem').find('.collapse:visible').each(function(i, elem){$(elem).trigger('click');});});
-	$('.shortView').find('.expand').each(function(i,ele){$(ele).click(function(){$(this).hide(); $(this).siblings('.collapse').show(); $(this).parents('.listItem').children('.mediumView').slideToggle('slow', function(){ if(($(this).parents('.itemList').find('.shortView').find('.expand:visible').length)==0){ $(this).parents('.itemList').find('.headerSwitchView').find('.expand').hide(); $(this).parents('.itemList').find('.headerSwitchView').find('.collapse').show();  }}     );})});
-	$('.shortView').find('.collapse').each(function(i,ele){$(ele).click(function(){$(this).hide(); $(this).siblings('.expand').show(); $(this).parents('.listItem').children('.mediumView').slideToggle('slow', function(){ if(($(this).parents('.itemList').find('.shortView').find('.collapse:visible').length)==0){ $(this).parents('.itemList').find('.headerSwitchView').find('.expand').show(); $(this).parents('.itemList').find('.headerSwitchView').find('.collapse').hide();  }}   );})});
+		
+	$('.headerSwitchView').find('.expand').click(
+			function(){
+				$(this).hide();
+				$(this).siblings('.collapse').show();
+				$(this).parents('.itemList').find('.listItem').find('.expand:visible').each(
+						function(i, elem){
+							$(elem).trigger('click');
+						});
+			});
+	$('.headerSwitchView').find('.collapse').click(
+			function(){
+				$(this).hide();
+				$(this).siblings('.expand').show();
+				$(this).parents('.itemList').find('.listItem').find('.collapse:visible').each(
+						function(i, elem){
+							$(elem).trigger('click');
+						});
+			});
+	
+	$('.shortView').find('.expand').each(
+			function(i,ele){
+				$(ele).click(
+						function(){
+							$(this).hide();
+							$(this).siblings('.collapse').show();
+							var parentElement = $(this).parents('.listItem');
+							$(parentElement).children('.mediumView').slideToggle('normal', function(){
+								if(($(parentElement).find('.shortView').find('.expand:visible').length)==0){
+									$(parentElement).find('.headerSwitchView').find('.expand').hide();
+									$(parentElement).find('.headerSwitchView').find('.collapse').show();
+								}
+							}
+							);
+						}
+				)
+			});
+	$('.shortView').find('.collapse').each(
+			function(i,ele){
+				$(ele).click(
+						function(){
+							$(this).hide();
+							$(this).siblings('.expand').show();
+							var parentElement = $(this).parents('.listItem');
+							$(parentElement).children('.mediumView').slideToggle('normal', function(){
+								if(($(parentElement).find('.shortView').find('.collapse:visible').length)==0){
+									$(parentElement).find('.headerSwitchView').find('.expand').show();
+									$(parentElement).find('.headerSwitchView').find('.collapse').hide();
+								}
+							});
+						}
+				)
+			});
 	
 }
 
