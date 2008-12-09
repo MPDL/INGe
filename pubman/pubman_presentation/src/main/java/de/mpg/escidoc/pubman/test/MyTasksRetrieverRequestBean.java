4c742940576b4aee37268eee6f035c4615c8040e
Markus Haarlaender <haarlaender@mpdl.mpg.de>
2008-12-09 07:56:00 +0000

    Bugfix for Moderator Workspace

--- pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/test/MyTasksRetrieverRequestBean.java
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/test/MyTasksRetrieverRequestBean.java
@@ -57,6 +57,7 @@ public class MyTasksRetrieverRequestBean extends MyItemsRetrieverRequestBean
         Navigation nav = (Navigation) getRequestBean(Navigation.class);
         nav.setShowExportMenuOption(true);
         
+        initSelectionMenu();
         
     }
     
