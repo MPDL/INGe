0ee846b02b8ecb823cbd98069fd0f45245c0feab
Bastien Saquet <saquet@mpdl.mpg.de>
2008-12-09 16:20:29 +0000

    mdsFacesContainer modified (method clone added)

--- common_services/common_logic/src/main/java/de/mpg/escidoc/services/common/valueobjects/face/MdsFacesContainerVO.java
+++ common_services/common_logic/src/main/java/de/mpg/escidoc/services/common/valueobjects/face/MdsFacesContainerVO.java
@@ -2,6 +2,7 @@ package de.mpg.escidoc.services.common.valueobjects.face;
 
 import de.mpg.escidoc.services.common.valueobjects.MetadataSetVO;
 import de.mpg.escidoc.services.common.valueobjects.metadata.CreatorVO;
+import de.mpg.escidoc.services.common.valueobjects.metadata.SourceVO;
 
 /**
  * Metadata content of a Faces container (album).
@@ -17,6 +18,35 @@ public class MdsFacesContainerVO extends MetadataSetVO
     private String name;
     private String description;
     private java.util.List<CreatorVO> creators = new java.util.ArrayList<CreatorVO>();
+    
+    
+    /**
+     * Default constructor.
+     */
+    public MdsFacesContainerVO()
+    {
+        
+    }
+    
+    /**
+     * Copy constructor.
+     * 
+     * @param other Another {@link MdsFacesContainerVO}.
+     */
+    public MdsFacesContainerVO(MdsFacesContainerVO other)
+    {
+        for (CreatorVO creator : other.creators)
+        {
+            this.creators.add((CreatorVO)creator.clone());
+        }
+        this.description = other.description;
+        this.name = other.name;
+    }
+    
+    public MdsFacesContainerVO clone()
+    {
+        return new MdsFacesContainerVO(this);
+    }
     
     public String getName()
     {
