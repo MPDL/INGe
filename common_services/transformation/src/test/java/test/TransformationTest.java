cba5723aaeb6a824ce05986c5d0c9131be1c5666
Friederike Kleinfercher <kleinfercher@mpdl.mpg.de>
2009-01-23 13:46:48 +0000

    reworked

--- common_services/transformation/src/test/java/test/TransformationTest.java
+++ common_services/transformation/src/test/java/test/TransformationTest.java
@@ -19,38 +19,50 @@ public class TransformationTest
     private final Logger logger = Logger.getLogger(TransformationTest.class);
     
     @Test
-    public void test()
+    public void test() throws Exception
     {
-        this.logger.info("Check sources xml:");
-        this.logger.info(this.trans.getSourceFormatsAsXml());
-        this.logger.info("Check target xml for escidoc item:");
-        this.logger.info(this.trans.getTargetFormatsAsXml("eSciDoc", "application/xml", "*"));
-        this.logger.info("Check target xml for escidocToc item:");
-        this.logger.info(this.trans.getTargetFormatsAsXml("eSciDocToc", "application/xml", "*"));
-        this.logger.info("Check source xml for escidoc item:");
-        Format[] tmp = this.trans.getSourceFormats(new Format ("eSciDoc", "application/xml", "UTF-8"));
-        this.logger.info(this.util.createFormatsXml(tmp));
         
-        this.logger.info("Check Transformation");
-        ClassLoader cl = this.getClass().getClassLoader();
-        InputStream in = cl.getResourceAsStream("test/resources/testFiles/escidocItem.xml");
-        
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-        StringBuilder stringBuilder = new StringBuilder();
-        String line = null;
+        try{
 
-        try
-        {
+            this.logger.debug("Check sources xml:");
+            this.trans.getSourceFormatsAsXml();
+            this.logger.debug("-----OK");
+            
+            this.logger.debug("Check target xml for escidoc item:");
+            this.trans.getTargetFormatsAsXml("eSciDoc", "application/xml", "*");
+            this.logger.debug("-----OK");
+            
+            this.logger.debug("Check target xml for escidocToc item:");
+            this.trans.getTargetFormatsAsXml("eSciDocToc", "application/xml", "*");
+            this.logger.debug("-----OK");
+            
+            this.logger.debug("Check source xml for escidoc item:");
+            Format[] tmp = this.trans.getSourceFormats(new Format ("eSciDoc", "application/xml", "UTF-8"));
+            this.util.createFormatsXml(tmp);
+            this.logger.debug("-----OK");
+            
+            this.logger.debug("Check Transformation");
+            ClassLoader cl = this.getClass().getClassLoader();
+            InputStream in = cl.getResourceAsStream("test/resources/testFiles/escidocItem.xml");
+            
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = null;
+
             while ((line = bufferedReader.readLine()) != null) 
             {
                 stringBuilder.append(line + "\n");
             }
-            this.logger.info(new String(this.trans.transform( stringBuilder.toString().getBytes("UTF-8"), 
-                    "escidoc", "application/xml", "UTF-8", "endnote", "text/plain", "UTF-8", "escidoc")));
+            new String(this.trans.transform( stringBuilder.toString().getBytes("UTF-8"), 
+                    "escidoc", "application/xml", "UTF-8", "endnote", "text/plain", "UTF-8", "escidoc"));
+            this.logger.debug("-----OK");
         }
         catch (Exception e)
         {
             this.logger.error("An error occurred during transformation",e);
+            throw new Exception(e);
         }
+        
+        this.logger.debug("Transformation Tests succeeded");
     }
 }
