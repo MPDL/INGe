fe66e4cf4f3e1372fbd3ab31bf7f743e7794df6e
Markus Haarlaender <haarlaender@mpdl.mpg.de>
2009-02-09 17:01:12 +0000

    Workaround: Changed sorting criteria sort.escidoc.complete-name to sort.escidoc.family-name until complete name is filled in items

--- pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/itemList/PubItemListSessionBean.java
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/itemList/PubItemListSessionBean.java
@@ -61,7 +61,7 @@ public class PubItemListSessionBean extends BasePaginatorListSessionBean<PubItem
         TITLE ("sort.escidoc.title", "/md-records/md-record/publication/title", OrderFilter.ORDER_ASCENDING),
         GENRE ("sort.escidoc.genre", "/md-records/md-record/publication/type", OrderFilter.ORDER_ASCENDING),
         DATE ("sort.escidoc.any-dates", "", OrderFilter.ORDER_DESCENDING),
-        CREATOR ("sort.escidoc.complete-name", "", OrderFilter.ORDER_ASCENDING),
+        CREATOR ("sort.escidoc.family-name", "", OrderFilter.ORDER_ASCENDING), //TODO: Change back to sort.escidoc.complete-name when complete name is filled!!
         PUBLISHING_INFO ("sort.escidoc.publisher", "/md-records/md-record/publication/source/publishing-info/publisher", OrderFilter.ORDER_ASCENDING),
         MODIFICATION_DATE ("sort.escidoc.last-modification-date", "/last-modification-date", OrderFilter.ORDER_DESCENDING),
         EVENT_TITLE ("sort.escidoc.any-event", "/md-records/md-record/publication/event/title", OrderFilter.ORDER_ASCENDING),
