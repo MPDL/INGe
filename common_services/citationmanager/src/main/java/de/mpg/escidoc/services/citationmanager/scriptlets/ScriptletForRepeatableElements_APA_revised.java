2ce491d90c1120e67fac55c253a7a8ad9828694d
Vladimir Makarenko <makarenko@mpdl.mpg.de>
2009-02-09 15:46:00 +0000

    new changes in APA_revised

--- common_services/citationmanager/src/main/java/de/mpg/escidoc/services/citationmanager/scriptlets/ScriptletForRepeatableElements_APA_revised.java
+++ common_services/citationmanager/src/main/java/de/mpg/escidoc/services/citationmanager/scriptlets/ScriptletForRepeatableElements_APA_revised.java
@@ -224,7 +224,112 @@ result += elem[0].length()>0 ? elem[0] + (es>1&&i<es-1?elem[1]:"") : "";
 elems.clear();
 return result;
 }
-public String getCS_1_PLE_9() throws Exception {
+public String getCS_1_PLE_6() throws Exception {
+String result = "";
+String str = "";
+String last = "";
+String delim = "";
+JRXmlDataSource ds = ((JRXmlDataSource) this.getParameterValue("REPORT_DATA_SOURCE"));
+JRXmlDataSource subDs = ds.subDataSource("md-record/publication/creator[@role='editor']/person|organization");
+JRDesignField field_default_0 = new JRDesignField();
+field_default_0.setDescription("family-name");
+field_default_0.setValueClass(String.class);
+String chunk_default_0 = "";
+
+JRDesignField field_default_1 = new JRDesignField();
+field_default_1.setDescription("given-name");
+field_default_1.setValueClass(String.class);
+String chunk_default_1 = "";
+
+JRDesignField field_last_0 = new JRDesignField();
+field_last_0.setDescription("family-name");
+field_last_0.setValueClass(String.class);
+String chunk_last_0 = "";
+
+JRDesignField field_last_1 = new JRDesignField();
+field_last_1.setDescription("given-name");
+field_last_1.setValueClass(String.class);
+String chunk_last_1 = "";
+
+int count = 1;
+boolean hasLast = false;
+while ( subDs.next() ) {
+
+ count++;
+switch (count) {
+default:
+chunk_default_0 = (String)subDs.getFieldValue(field_default_0);chunk_default_0 = xmlEncode(chunk_default_0);chunk_default_0 = chunk_default_0!=null && chunk_default_0.length()>0 ? chunk_default_0 : "";chunk_default_0 = chunk_default_0.length()>0 ? (chunk_default_0) : ""; chunk_default_0 = (chunk_default_0);chunk_default_0 = chunk_default_0;
+chunk_default_1 = (String)subDs.getFieldValue(field_default_1);chunk_default_1 = xmlEncode(chunk_default_1);chunk_default_1 = chunk_default_1!=null && chunk_default_1.length()>0 ? chunk_default_1 : "";chunk_default_1 = get_initials(chunk_default_1);chunk_default_1 = chunk_default_1.length()>0 ? (chunk_default_1) : ""; chunk_default_1 = get_initials(chunk_default_1);chunk_default_1 = chunk_default_1;
+str = chunk_default_0 + insertDelimiter(chunk_default_0, ", ", chunk_default_1) + chunk_default_1;elems.add( new String[]{ str, ", " } );
+break;
+}
+if (!hasLast) hasLast = true;
+if (hasLast) {
+chunk_last_0 = (String)subDs.getFieldValue(field_last_0);chunk_last_0 = xmlEncode(chunk_last_0);chunk_last_0 = chunk_last_0!=null && chunk_last_0.length()>0 ? chunk_last_0 : "";chunk_last_0 = chunk_last_0.length()>0 ? (chunk_last_0) : ""; chunk_last_0 = (chunk_last_0);chunk_last_0 = chunk_last_0;
+chunk_last_1 = (String)subDs.getFieldValue(field_last_1);chunk_last_1 = xmlEncode(chunk_last_1);chunk_last_1 = chunk_last_1!=null && chunk_last_1.length()>0 ? chunk_last_1 : "";chunk_last_1 = get_initials(chunk_last_1);chunk_last_1 = chunk_last_1.length()>0 ? (chunk_last_1) : ""; chunk_last_1 = get_initials(chunk_last_1);chunk_last_1 = chunk_last_1;
+last = chunk_last_0 + insertDelimiter(chunk_last_0, ", ", chunk_last_1) + chunk_last_1;delim = ", &amp; ";}
+
+}
+int es = elems.size();
+if ( hasLast && count>2 && ( !subDs.next( ) ) ) {
+int idx = es - ( 2 ) ;
+String[] elem = (String[])elems.get( idx );
+elems.set(idx + 1, new String[]{ last, "" });
+elem[1] = delim;
+elems.set(idx, elem);
+
+}
+for(int i=0; i<es; i++) {
+String[] elem = (String[])elems.get(i);
+result += elem[0].length()>0 ? elem[0] + (es>1&&i<es-1?elem[1]:"") : "";
+}
+elems.clear();
+return result;
+}
+public String getCS_1_PLE_7() throws Exception {
+String result = "";
+String str = "";
+String last = "";
+String delim = "";
+JRXmlDataSource ds = ((JRXmlDataSource) this.getParameterValue("REPORT_DATA_SOURCE"));
+JRXmlDataSource subDs = ds.subDataSource("md-record/publication/creator[@role='editor']/person|organization");
+JRDesignField field_default_0 = new JRDesignField();
+field_default_0.setDescription("family-name");
+field_default_0.setValueClass(String.class);
+String chunk_default_0 = "";
+
+JRDesignField field_default_1 = new JRDesignField();
+field_default_1.setDescription("given-name");
+field_default_1.setValueClass(String.class);
+String chunk_default_1 = "";
+
+int count = 1;
+boolean hasLast = false;
+boolean maxCount = false;
+away: while ( subDs.next() ) {
+if ( count >6) {
+ maxCount = true;
+ break away;
+}
+ count++;
+switch (count) {
+default:
+chunk_default_0 = (String)subDs.getFieldValue(field_default_0);chunk_default_0 = xmlEncode(chunk_default_0);chunk_default_0 = chunk_default_0!=null && chunk_default_0.length()>0 ? chunk_default_0 : "";chunk_default_0 = chunk_default_0.length()>0 ? (chunk_default_0) : ""; chunk_default_0 = (chunk_default_0);chunk_default_0 = chunk_default_0;
+chunk_default_1 = (String)subDs.getFieldValue(field_default_1);chunk_default_1 = xmlEncode(chunk_default_1);chunk_default_1 = chunk_default_1!=null && chunk_default_1.length()>0 ? chunk_default_1 : "";chunk_default_1 = get_initials(chunk_default_1);chunk_default_1 = chunk_default_1.length()>0 ? (chunk_default_1) : ""; chunk_default_1 = get_initials(chunk_default_1);chunk_default_1 = chunk_default_1;
+str = chunk_default_0 + insertDelimiter(chunk_default_0, ", ", chunk_default_1) + chunk_default_1;elems.add( new String[]{ str, ", " } );
+break;
+}
+
+}
+int es = elems.size();
+for(int i=0; i<es; i++) {
+String[] elem = (String[])elems.get(i);
+result += elem[0].length()>0 ? elem[0] + (es>1&&i<es-1?elem[1]:"") : "";
+}
+elems.clear();
+return result;
+}
+public String getCS_1_PLE_11() throws Exception {
 String result = "";
 String str = "";
 String last = "";
@@ -286,7 +391,7 @@ result += elem[0].length()>0 ? elem[0] + (es>1&&i<es-1?elem[1]:"") : "";
 elems.clear();
 return result;
 }
-public String getCS_1_PLE_8() throws Exception {
+public String getCS_1_PLE_10() throws Exception {
 String result = "";
 String str = "";
 String last = "";
