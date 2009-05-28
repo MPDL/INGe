666b6f807b55535d819614a1d15acb299c5abdb3
tendres <tendres@localhost>
2009-05-28 14:14:16 +0000

    added installer module

---
+++ pubman/pubman_installer/src/main/java/de/mpg/escidoc/pubman/installer/Installer.java
@@ -0,0 +1,76 @@
+/*
+* CDDL HEADER START
+*
+* The contents of this file are subject to the terms of the
+* Common Development and Distribution License, Version 1.0 only
+* (the "License"). You may not use this file except in compliance
+* with the License.
+*
+* You can obtain a copy of the license at license/ESCIDOC.LICENSE
+* or http://www.escidoc.de/license.
+* See the License for the specific language governing permissions
+* and limitations under the License.
+*
+* When distributing Covered Code, include this CDDL HEADER in each
+* file and include the License file at license/ESCIDOC.LICENSE.
+* If applicable, add the following below this CDDL HEADER, with the
+* fields enclosed by brackets "[]" replaced with your own identifying
+* information: Portions Copyright [yyyy] [name of copyright owner]
+*
+* CDDL HEADER END
+*/
+
+/*
+* Copyright 2006-2007 Fachinformationszentrum Karlsruhe Gesellschaft
+* für wissenschaftlich-technische Information mbH and Max-Planck-
+* Gesellschaft zur Förderung der Wissenschaft e.V.
+* All rights reserved. Use is subject to license terms.
+*/
+package de.mpg.escidoc.pubman.installer;
+
+
+//import org.apache.log4j.Logger;
+
+
+/**
+ * @author endres
+ *
+ */
+public class Installer
+{
+
+    /**
+     * @param args
+     */
+    
+//    private Logger logger = null;
+    
+    public Installer() {
+//        logger = Logger.getLogger(Installer.class);
+    }
+    
+    public static void main(String[] args)
+    {
+        Installer installer = new Installer();
+        System.out.println("Huhuuu!");
+//        try
+//        {
+//           URL frameworkUrl = new URL("http://framework:8080");
+//            ServiceLocator.getSearchHandler("test", frameworkUrl);
+//            installer.logger.info("setup complete");
+//        } catch (ServiceException e)
+//        {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        } catch (URISyntaxException e)
+//        {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        } catch (MalformedURLException e)
+//        {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+    }
+
+}
