3621532b5f26b3fe1ea181690ee059a42ca53098
Friederike Kleinfercher <kleinfercher@mpdl.mpg.de>
2009-11-24 08:16:13 +0000

    gets citations again from citation manager, not from transformation manager

--- common_services/search/src/main/java/de/mpg/escidoc/services/search/bean/SearchBean.java
+++ common_services/search/src/main/java/de/mpg/escidoc/services/search/bean/SearchBean.java
@@ -424,9 +424,8 @@ public class SearchBean implements Search
         // structured export
         if (formatType == FormatType.LAYOUT)
         {
-            //TODO call only transformation service
             logger.info("Calling citationStyleHandler");
-            exportData = citationStyleHandler.getOutput(exportFormat, itemList);
+            exportData = citationStyleHandler.getOutput(exportFormat, outputFormat, itemList);
             logger.info("Returning from citationStyleHandler");
         } 
         else if (formatType == FormatType.STRUCTURED)
