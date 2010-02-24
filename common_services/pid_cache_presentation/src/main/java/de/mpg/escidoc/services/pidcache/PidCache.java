d8b84244dce4c53fb9168bc3428cb5eb960222cc
Michael Franke <franke@mpdl.mpg.de>
2010-02-24 13:49:39 +0000

    Initial creation

---
+++ common_services/pid_cache_presentation/src/main/java/de/mpg/escidoc/services/pidcache/PidCache.java
@@ -0,0 +1,61 @@
+/*
+*
+* CDDL HEADER START
+*
+* The contents of this file are subject to the terms of the
+* Common Development and Distribution License, Version 1.0 only
+* (the "License"). You may not use this file except in compliance
+* with the License.
+*
+* You can obtain a copy of the license at license/ESCIDOC.LICENSE
+* or http://www.escidoc.de/license.
+* See the License for the specific language governing permissions
+* and limitations under the License.
+*
+* When distributing Covered Code, include this CDDL HEADER in each
+* file and include the License file at license/ESCIDOC.LICENSE.
+* If applicable, add the following below this CDDL HEADER, with the
+* fields enclosed by brackets "[]" replaced with your own identifying
+* information: Portions Copyright [yyyy] [name of copyright owner]
+*
+* CDDL HEADER END
+*/
+
+/*
+* Copyright 2006-2007 Fachinformationszentrum Karlsruhe Gesellschaft
+* für wissenschaftlich-technische Information mbH and Max-Planck-
+* Gesellschaft zur Förderung der Wissenschaft e.V.
+* All rights reserved. Use is subject to license terms.
+*/ 
+
+package de.mpg.escidoc.services.pidcache;
+
+/**
+ * TODO Description
+ *
+ * @author franke (initial creation)
+ * @author $Author$ (last modification)
+ * @version $Revision$ $LastChangedDate$
+ *
+ */
+public class PidCache
+{
+
+    /**
+     * This method does the following:
+     *  - Take a Pid from the cache
+     *  - Invoke the following (asynchroous) workflow:
+     *      - Change the URL of the Pid in the Handle system.
+     *      - Add a new PID to the cache.
+     *  - Return the Pid
+     * 
+     * @param url The URL to be registered.
+     * 
+     * @return The Pid.
+     */
+    public Pid assignPid(String url)
+    {
+        return null;
+    }
+    
+}
