04f086c469c036312e49d1d440ba71b3d90f8c54
Bastien Saquet <saquet@mpdl.mpg.de>
2010-03-10 13:52:57 +0000

    Ready for testing on dev

---
+++ common_services/pid_cache_ear/src/test/java/test/pidcache/PidCacheServiceTest.java
@@ -0,0 +1,111 @@
+package test.pidcache;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import javax.naming.InitialContext;
+
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.mpg.escidoc.services.common.XmlTransforming;
+import de.mpg.escidoc.services.common.valueobjects.PidServiceResponseVO;
+import de.mpg.escidoc.services.framework.PropertyReader;
+
+
+
+/**
+ * Test class for Pid Cache Service
+ * @author saquet
+ *
+ */
+public class PidCacheServiceTest 
+{
+	private static final Logger logger = Logger.getLogger(PidCacheServiceTest.class);
+	
+	private static HttpClient client;
+	private static XmlTransforming xmlTransforming = null;
+	
+	public static String PIDSERVICE_CREATE;
+    public static String PIDSERVICE_VIEW ;
+    public static String PIDSERVICE_FIND ;
+    public static String CACHE_PIDSERVICE ;
+	public static String PIDSERVICE_EDIT ;
+	public static String PIDSERVICE_DELETE ;
+	public static String ITEM_TEST_URL;
+	
+	
+	public String testUrl= null;
+	public String testIdentifier = null;
+
+	
+	@BeforeClass
+	public static void setProperties() throws Exception
+	{
+		System.out.println("before");
+		InitialContext context = new InitialContext();
+    	xmlTransforming = (XmlTransforming)context.lookup(XmlTransforming.SERVICE_NAME);
+    	
+		CACHE_PIDSERVICE = PropertyReader.getProperty("escidoc.pid.pidcache.service.url");
+		PIDSERVICE_CREATE = PropertyReader.getProperty("escidoc.pid.service.create.path");
+    	PIDSERVICE_VIEW = PropertyReader.getProperty("escidoc.pid.service.view.path");
+    	PIDSERVICE_FIND = PropertyReader.getProperty("escidoc.pid.service.search.path");
+    	PIDSERVICE_EDIT = PropertyReader.getProperty("escidoc.pid.service.update.path");
+    	PIDSERVICE_DELETE = PropertyReader.getProperty("escidoc.pid.service.delete.path");
+    	ITEM_TEST_URL = PropertyReader.getProperty("escidoc.pidcache.dummy.url");
+   
+    	client = new HttpClient();  	
+	}
+	
+	@Test
+	public void init() throws Exception
+	{	
+		testUrl = ITEM_TEST_URL.concat(Long.toString(new Date().getTime())).concat("/test");
+		this.testAssignPid();
+		this.testUpdatePid();
+	}
+	
+	public void testAssignPid() throws Exception
+	{
+		logger.info("Test assign PID for url: " + testUrl + "...");
+		PostMethod method = new PostMethod(CACHE_PIDSERVICE.concat(PIDSERVICE_CREATE));
+		method.setParameter("url", testUrl);
+    	client.executeMethod(method);
+		PidServiceResponseVO pidServiceResponseVO = xmlTransforming.transformToPidServiceResponse(method.getResponseBodyAsString());
+		testIdentifier = pidServiceResponseVO.getIdentifier();
+		assertNotNull("PID pidServiceResponseVO.getIdentifier()" + " has been assigned.", testIdentifier);
+		logger.info("done.");
+	}
+	
+	public void testUpdatePid() throws Exception
+	{
+		testUrl = testUrl.concat("/edition");
+		logger.info("Test Update PID " + testIdentifier + " with url: " + testUrl + "...");
+		PostMethod method = new PostMethod(CACHE_PIDSERVICE.concat(PIDSERVICE_EDIT).concat("?pid=").concat(testIdentifier));
+		method.setParameter("url", testUrl);
+    	client.executeMethod(method);
+		PidServiceResponseVO pidServiceResponseVO = xmlTransforming.transformToPidServiceResponse(method.getResponseBodyAsString());
+		assertEquals(testIdentifier, pidServiceResponseVO.getIdentifier());
+		assertEquals(testUrl, pidServiceResponseVO.getUrl());
+		logger.info("done.");
+	}
+	
+	public void testAssignPidGwdgNonAvailable()
+	{
+		logger.info("GWDG not available: Test Create PID with url: " + testUrl + ".");
+	}
+	
+	public void testUpdatePidGwdgNonAvailable() throws Exception
+	{
+		logger.info("GWDG not available: Test Update PID " + testIdentifier + " with url: " + testUrl + ".");
+	}
+
+}
