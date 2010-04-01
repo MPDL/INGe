d21e9b0e44abeac300a0aa8be00e0bcebae4a7c6
Vladimir Makarenko <makarenko@mpdl.mpg.de>
2010-04-01 10:39:47 +0000

    test cases for revised endnote import

--- common_services/transformation/src/test/java/test/EndNoteImportTest.java
+++ common_services/transformation/src/test/java/test/EndNoteImportTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.List;
 
@@ -26,6 +27,7 @@ public class EndNoteImportTest
 	Transformation transformation;
 	private Util util = new Util();
 	
+    private Format inputFormatICE = new Format("EndNote-ICE", "text/plain", "UTF-8");
     private Format inputFormat = new Format("EndNote", "text/plain", "UTF-8");
     private Format outputFormat = new Format("escidoc-publication-item-list", "application/xml", "UTF-8");
 
@@ -39,10 +41,10 @@ public class EndNoteImportTest
     } 
 	
 	@Test
-	@Ignore
+//	@Ignore
 	public void CheckEndNoteImport() throws Exception
 	{  	
-    	InputStream inputStream = ResourceUtil.getResourceAsStream("./src/test/resources/testFiles/endnote/publikationsliste_2008_endnote.txt");
+    	InputStream inputStream = ResourceUtil.getResourceAsStream("testFiles/endnote/publikationsliste_2008_endnote.txt");
     	ByteArrayOutputStream baos = new ByteArrayOutputStream();
     	byte[] buffer = new byte[2048];
     	int read;
@@ -56,15 +58,35 @@ public class EndNoteImportTest
 	}
 	
 	@Test
-	public void checkEndnoteListTransformation() throws Exception
+	@Ignore
+	public void checkEndnoteICEListTransformation() throws Exception
 	{
-	    this.logger.info("Transform EndNote list to escidoc list");
+	    this.logger.info("Transform EndNote ICE list to escidoc list");
 	    
        	byte[] result = this.transformation.transform(this.util.getResourceAsString("testFiles/endnote/publikationsliste_2008_endnote2.txt").getBytes(), 
-       	        this.inputFormat, this.outputFormat, "escidoc");
+       	        this.inputFormatICE, this.outputFormat, "escidoc");
        	XmlTransformingBean xmlTransforming = new XmlTransformingBean();
        	List<PubItemVO> itemList = (List<PubItemVO>)xmlTransforming.transformToPubItemList(new String(result, "UTF-8"));
        	Assert.assertNotNull(itemList);
+	}
+	
+	@Test
+	public void checkEndnoteListTransformation() throws Exception
+	{
+		this.logger.info("Transform EndNote list to escidoc list. New implemetation");
+		
+		byte[] result = this.transformation.transform(this.util.getResourceAsString("testFiles/endnote/EndNote_Import_revised_implementation.txt").getBytes(), 
+				this.inputFormat, this.outputFormat, "escidoc");
+		
+//		FileOutputStream fos = new FileOutputStream("target/endnote_transformed.xml");
+//    	fos.write(result);
+//    	fos.close();
+		
+		XmlTransformingBean xmlTransforming = new XmlTransformingBean();
+		List<PubItemVO> itemList = (List<PubItemVO>)xmlTransforming.transformToPubItemList(new String(result, "UTF-8"));
+		Assert.assertNotNull(itemList);
+		
+//		logger.info(new String(result,"UTF-8"));
 	}
 
 }
