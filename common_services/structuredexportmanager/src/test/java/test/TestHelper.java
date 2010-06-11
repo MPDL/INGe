2e6d1966c46faad89336636ef7401d491e89861a
Vladimir Makarenko <makarenko@mpdl.mpg.de>
2010-06-11 14:13:48 +0000

    framework_access AdminHelper.loginUser usage

--- common_services/structuredexportmanager/src/test/java/test/TestHelper.java
+++ common_services/structuredexportmanager/src/test/java/test/TestHelper.java
@@ -56,6 +56,7 @@ import org.apache.commons.httpclient.cookie.CookieSpec;
 import org.apache.commons.httpclient.methods.PostMethod;
 
 import de.escidoc.www.services.om.ItemHandler;
+import de.mpg.escidoc.services.framework.AdminHelper;
 import de.mpg.escidoc.services.framework.ServiceLocator;
 
 
@@ -149,7 +150,7 @@ public class TestHelper
     public static String getItemListFromFramework() throws IOException, ServiceException, URISyntaxException
     {
     	
-    	String userHandle = loginUser("test_dep_scientist", "verdi"); 
+    	String userHandle = AdminHelper.loginUser("test_dep_scientist", "verdi"); 
         ItemHandler ch = ServiceLocator.getItemHandler(userHandle);
         // see here for filters: https://zim02.gwdg.de/repos/common/trunk/common_services/common_logic/src/main/java/de/mpg/escidoc/services/common/xmltransforming/JiBXFilterTaskParamVOMarshaller.java
         String filter = 
@@ -163,72 +164,5 @@ public class TestHelper
     
     }
     
-    
-    
-    protected static String loginUser(String userid, String password) throws HttpException, IOException, ServiceException, URISyntaxException
-    {
-    	String frameworkUrl = ServiceLocator.getFrameworkUrl();
-    	StringTokenizer tokens = new StringTokenizer( frameworkUrl, "//" );
-    	if( tokens.countTokens() != 2 ) {
-    		throw new IOException( "Url in the config file is in the wrong format, needs to be http://<host>:<port>" );
-    	}
-    	tokens.nextToken();
-    	StringTokenizer hostPort = new StringTokenizer(tokens.nextToken(), ":");
-
-    	if( hostPort.countTokens() != 2 ) {
-    		throw new IOException( "Url in the config file is in the wrong format, needs to be http://<host>:<port>" );
-    	}
-    	String host = hostPort.nextToken();
-    	int port = Integer.parseInt( hostPort.nextToken() );
-
-    	HttpClient client = new HttpClient();
-
-    	client.getHostConfiguration().setHost( host, port, "http");
-    	client.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
-
-    	PostMethod login = new PostMethod( frameworkUrl + "/aa/j_spring_security_check");
-    	login.addParameter("j_username", userid);
-    	login.addParameter("j_password", password);
-
-    	client.executeMethod(login);
-
-    	login.releaseConnection();
-    	CookieSpec cookiespec = CookiePolicy.getDefaultSpec();
-    	Cookie[] logoncookies = cookiespec.match(
-    			host, port, "/", false, 
-    			client.getState().getCookies());
-
-    	Cookie sessionCookie = logoncookies[0];
-
-    	PostMethod postMethod = new PostMethod("/aa/login");
-    	postMethod.addParameter("target", frameworkUrl);
-    	client.getState().addCookie(sessionCookie);
-    	client.executeMethod(postMethod);
-
-    	if (HttpServletResponse.SC_SEE_OTHER != postMethod.getStatusCode())
-    	{
-    		throw new HttpException("Wrong status code: " + login.getStatusCode());
-    	}
-
-    	String userHandle = null;
-    	Header headers[] = postMethod.getResponseHeaders();
-    	for (int i = 0; i < headers.length; ++i)
-    	{
-    		if ("Location".equals(headers[i].getName()))
-    		{
-    			String location = headers[i].getValue();
-    			int index = location.indexOf('=');
-    			userHandle = new String(Base64.decode(location.substring(index + 1, location.length())));
-    			//System.out.println("location: "+location);
-    			//System.out.println("handle: "+userHandle);
-    		}
-    	}
-
-    	if (userHandle == null)
-    	{
-    		throw new ServiceException("User not logged in.");
-    	}
-    	return userHandle;
-    }
     
 }
