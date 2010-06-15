3bb3640a949e1804fc99fd4aa52cc285536b9795
Vladimir Makarenko <makarenko@mpdl.mpg.de>
2010-06-15 14:53:30 +0000

    + UnapiTest.java

---
+++ common_services/dataacquisition_ear/src/test/java/de/mpg/escidoc/services/test/dataacquisition_ear/UnapiTest.java
@@ -0,0 +1,145 @@
+package de.mpg.escidoc.services.test.dataacquisition_ear;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import de.mpg.escidoc.services.framework.PropertyReader;
+import de.mpg.escidoc.services.framework.ProxyHelper;
+
+/**
+ * These tests check the unapi interface for the dataaquisition service.
+ * Please make sure that dev-pubman is running!
+ * (Otherwise change the 'location' path or skip this test)
+ *
+ * @author kleinfe1 (initial creation)
+ * @author $Author$ (last modification)
+ * @version $Revision$ $LastChangedDate$
+ *
+ */
+public class UnapiTest
+{
+    private Logger logger = Logger.getLogger(UnapiTest.class);
+    
+    private static String location;
+    private String arxivId = "arXiv:0904.3933";
+    private String pmcId = "pmc:PMC2043518";
+    private String bmcId = "bmc:1472-6890-9-1";
+    private String spiresId ="spires:hep-ph/0001001";
+
+    @BeforeClass
+    public static void Init() throws IOException, URISyntaxException
+    {	
+    	location = PropertyReader.getProperty("escidoc.dataacquisition.service.url") + "download/unapi";
+    }
+    
+    @Test
+    public void unapiTestAllSources() throws Exception
+    {
+        HttpClient client = new HttpClient();
+        String url = "";
+        int code = 0;
+        
+        //arxiv
+        url = this.location + "?id=" + this.arxivId + "&format=arxiv";
+        GetMethod getMethod = new GetMethod(url); 
+        
+        
+        code = ProxyHelper.executeMethod(client, getMethod);
+        this.logger.info("Fetch: " + url + "    Response: " + code);
+        Assert.assertEquals(200, code);
+        
+        //pmc
+        url = this.location + "?id=" + this.pmcId + "&format=pmc";
+        getMethod = new GetMethod(url);            
+        code = ProxyHelper.executeMethod(client, getMethod);
+        this.logger.info("Fetch: " + url + "    Response: " + code);
+        Assert.assertEquals(200, code);
+        
+        //bmc
+        url = this.location + "?id=" + this.bmcId + "&format=bmc";
+        getMethod = new GetMethod(url);            
+        code = ProxyHelper.executeMethod(client, getMethod);
+        this.logger.info("Fetch: " + url + "    Response: " + code);
+        Assert.assertEquals(200, code);
+        
+        //spires
+        url = this.location + "?id=" + this.spiresId + "&format=spires";
+        getMethod = new GetMethod(url);            
+        code = ProxyHelper.executeMethod(client, getMethod);
+        this.logger.info("Fetch: " + url + "    Response: " + code);
+        Assert.assertEquals(200, code);
+    }
+    
+    @Test
+    //On arxiv example
+    public void unapiTestOneSourceFull() throws Exception
+    {
+        HttpClient client = new HttpClient();
+        String url = "";
+        List <String> formatsList = new ArrayList<String> ();
+        
+        //Get all formats for arxiv
+        url = this.location + "?id=arxiv";
+        GetMethod getMethod = new GetMethod(url); 
+        ProxyHelper.executeMethod(client, getMethod);
+        String formatsXml = new String (getMethod.getResponseBody(), "UTF-8");
+        formatsList = this.formatsHelper(formatsXml);
+        logger.info(url);
+        logger.info(formatsXml);
+
+        
+        //Retrieve item in all formats
+        for (int i = 0; i < formatsList.size(); i ++)
+        {
+            url = this.location + "?id=" + this.arxivId + "&format=" + formatsList.get(i);
+            getMethod = new GetMethod(url);            
+            int code = ProxyHelper.executeMethod(client, getMethod);
+            this.logger.info("Fetch: " + url + "    Response: " + code);
+            Assert.assertEquals(200, code);
+        }
+    }
+    
+    private List <String> formatsHelper(String formatsXml) throws Exception
+    {
+        List <String> formatsList = new ArrayList<String> ();
+        
+        //parse xml
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        
+        InputSource inStream = new InputSource();
+        inStream.setCharacterStream(new StringReader(formatsXml));
+        Document doc = db.parse(inStream);
+        doc.getDocumentElement().normalize();
+        NodeList nodeLst = doc.getElementsByTagName("format");
+        
+        for (int i = 0; i < nodeLst.getLength(); i++) 
+        {
+            Node fstNode = nodeLst.item(i);
+            Element fstElmnt = (Element) fstNode;
+            Node attr = fstElmnt.getAttributeNode("name");
+            formatsList.add(attr.getNodeValue());
+        }
+        
+        return formatsList;
+    }
+    
+}
