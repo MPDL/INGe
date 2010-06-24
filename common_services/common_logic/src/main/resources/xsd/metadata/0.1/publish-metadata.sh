164b1b3761fd7eba58ff54a14afcb79541502b23
Natasa Bulatovic <bulatovic@mpdl.mpg.de>
2010-06-24 14:24:28 +0000

    updated scripts with some more information

--- common_services/common_logic/src/main/resources/xsd/metadata/0.1/publish-metadata.sh
+++ common_services/common_logic/src/main/resources/xsd/metadata/0.1/publish-metadata.sh
@@ -1,11 +1,21 @@
 #! /bin/sh
-##NOTE: this file is only per metadata version profile to avoid unintended overwriting
-###use only when first-time check-out of the metadata profile
-#mkdir /srv/metadata/0.1
-#svn co https://zim02.gwdg.de/repos/common/trunk/common_services/common_logic/src/main/resources/xsd/metadata/0.1
-#check-out latest changes
-
-#use when publishing the metadata changes
+#####  Author: Natasa Bulatovic,24.06.2010
+#####  NOTE: this file is only per metadata version profile to avoid unintended overwriting
+#####
+#####  STEP 1
+#####
+#####   The commands in the next 2 lines need to be used only when first-time check-out of the metadata profile
+#####     mkdir /srv/metadata/0.1
+#####     svn co https://zim02.gwdg.de/repos/common/trunk/common_services/common_logic/src/main/resources/xsd/metadata/0.1
+#####
+#####
+#####
+#####  STEP 2 
+#####  use when publishing the metadata changes
+#####  executes another shell-script. Both can be used independently depending on  
+#####  what needs to be done. 
+#####
+#####
 echo "Updating from SVN"
 svn up
 echo "Preparing directories to replace relative paths..."
@@ -16,6 +26,5 @@ cp *.xsd /srv/metadata/0.1/replacements/.
 echo "Replacing paths..."
 ./replace_paths.sh
 echo "Moving to web-space ...."
-#cp ./replacements/*.xsd /srv/www/metadata/escidoc/metadata/schemas/0.1/.
+cp ./replacements/*.xsd /srv/www/metadata/escidoc/metadata/schemas/0.1/.
 echo "Finished...- make sure you test the profiles with real metadata records"
-
--- common_services/common_logic/src/main/resources/xsd/metadata/0.1/replace_paths.sh
+++ common_services/common_logic/src/main/resources/xsd/metadata/0.1/replace_paths.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+####    Author: Natasa Bulatovic, 24.06.2010
+####    Has defined old and new string. This script shall be run in the current directory
+####    where the schemas for the metadata profile are checked-out
+####    it creates new directory for the replaced files and a new subdirectory for the original files (just in case)
+####    both "replacement" and "backup" directory may be removed.
+####    This script may be called independently or by publish_metadata.sh
+####    moving of the metadata profiles is done in publish_metadata.sh
+####
 OLD="..\/..\/metadata\/0.1\/"
 NEW="http:\/\/metadata.mpdl.mpg.de\/escidoc\/metadata\/schemas\/0.1\/"
 DPATH="./replacements/*.xsd"
