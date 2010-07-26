70f1dd04f56d9595052fb37019469d5a4e666455
Michael Franke <franke@mpdl.mpg.de>
2010-07-26 15:39:35 +0000

    Added replace pattern class

---
+++ common_services/cone/src/main/java/de/mpg/escidoc/services/cone/util/ReplacePattern.java
@@ -0,0 +1,81 @@
+/*
+*
+* CDDL HEADER START
+*
+* The contents of this file are subject to the terms of the
+* Common Development and Distribution License, Version 1.0 only
+* (the "License"). You may not use this file except in compliance
+* with the License.
+*
+* You can obtain a copy of the license at license/ESCIDOC.LICENSE
+* or http://www.escidoc.de/license.
+* See the License for the specific language governing permissions
+* and limitations under the License.
+*
+* When distributing Covered Code, include this CDDL HEADER in each
+* file and include the License file at license/ESCIDOC.LICENSE.
+* If applicable, add the following below this CDDL HEADER, with the
+* fields enclosed by brackets "[]" replaced with your own identifying
+* information: Portions Copyright [yyyy] [name of copyright owner]
+*
+* CDDL HEADER END
+*/
+
+/*
+* Copyright 2006-2010 Fachinformationszentrum Karlsruhe Gesellschaft
+* für wissenschaftlich-technische Information mbH and Max-Planck-
+* Gesellschaft zur Förderung der Wissenschaft e.V.
+* All rights reserved. Use is subject to license terms.
+*/
+
+package de.mpg.escidoc.services.cone.util;
+
+import java.util.regex.Pattern;
+
+/**
+ * Helper class for result pattern.
+ *
+ * @author franke (initial creation)
+ * @author $Author: mfranke $ (last modification)
+ * @version $Revision: 3229 $ $LastChangedDate: 2010-06-14 13:19:44 +0200 (Mo, 14 Jun 2010) $
+ *
+ */
+class ReplacePattern
+{
+    Pattern pattern;
+    String replace;
+    
+    /**
+     * Convenience cvonstructor.
+     * 
+     * @param patternString Will be converted to a @see java.util.regex.Pattern
+     * @param replace The string the matching pattern will be substituted by.
+     */
+    public ReplacePattern(String patternString, String replace)
+    {
+        this.pattern = Pattern.compile(patternString);
+        this.replace = replace;
+    }
+
+    public Pattern getPattern()
+    {
+        return pattern;
+    }
+
+    public void setPattern(Pattern pattern)
+    {
+        this.pattern = pattern;
+    }
+
+    public String getReplace()
+    {
+        return replace;
+    }
+
+    public void setReplace(String replace)
+    {
+        this.replace = replace;
+    }
+    
+}
+
