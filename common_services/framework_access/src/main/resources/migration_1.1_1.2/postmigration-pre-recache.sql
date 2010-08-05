ed16fc25f5ee72ec82e2b18be894682794eeb661
Natasa Bulatovic <bulatovic@mpdl.mpg.de>
2010-08-05 10:59:50 +0000

    NOTE ADDED

--- common_services/framework_access/src/main/resources/migration_1.1_1.2/postmigration-pre-recache.sql
+++ common_services/framework_access/src/main/resources/migration_1.1_1.2/postmigration-pre-recache.sql
@@ -1,3 +1,4 @@
+--NOTE: THIS SCRIPT MUST BE RUN AFTER MIGRATION AND BEFORE RECACHING
 drop index list.id_path_position;
 DROP INDEX list.local_path_public_status_value_index;
 DROP INDEX list.local_path_version_status_value_index;
@@ -8,6 +9,8 @@ DROP INDEX list.path_id_position;
 DROP INDEX list.path_parents_value_index;
 DROP INDEX list.path_structmap_container_index;
 DROP INDEX list.path_structmap_index;
+
+--AFTER THESE INDEXES ARE DROPPED CHECK IF THERE IS SOME INDEX LEFT; DROP IT MANUALLY IF NEEDED
 vacuum analyze list.property;
 
 CREATE INDEX group_role_date_object_role_grant_idx
