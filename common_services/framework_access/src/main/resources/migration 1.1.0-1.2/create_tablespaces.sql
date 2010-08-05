757f634249afa03813bfac1849363736e5500baa
Natasa Bulatovic <bulatovic@mpdl.mpg.de>
2010-08-05 08:46:13 +0000

    create tablespaces (new structure to redistribute to two disks data and indexes)

---
+++ common_services/framework_access/src/main/resources/migration 1.1.0-1.2/create_tablespaces.sql
@@ -0,0 +1,29 @@
+﻿--creation of the tablespaces
+CREATE TABLESPACE tbl_fedora_data
+  OWNER postgres
+  LOCATION '/var/lib/pgsql/data/tables/fedora';
+
+CREATE TABLESPACE tbl_fedora_index
+  OWNER postgres
+  LOCATION '/var/lib/pgsql/data/indexes/fedora';
+
+CREATE TABLESPACE tbl_triples_data
+  OWNER postgres
+  LOCATION '/var/lib/pgsql/data/tables/triples';
+
+CREATE TABLESPACE tbl_triples_index
+  OWNER postgres
+  LOCATION '/var/lib/pgsql/data/indexes/triples';
+
+CREATE TABLESPACE tbl_escidoc_core_data
+  OWNER postgres
+  LOCATION '/var/lib/pgsql/data/tables/escidoc-core';
+
+CREATE TABLESPACE tbl_escidoc_core_large_index
+  OWNER postgres
+  LOCATION '/var/lib/pgsql/data/indexes/escidoc-core-large';
+
+CREATE TABLESPACE tbl_escidoc_core_normal_index
+  OWNER postgres
+  LOCATION '/var/lib/pgsql/data/indexes/escidoc-core-normal';
+
