67864402e1a4b03379ebca07ed7b3819aee26ab5
Lu Yu <yu@mpdl.mpg.de>
2010-11-30 16:34:15 +0000

    Former-commit-id: b6622ff0a20f9e06e7e9bbf1fa62d5676f2e7090

--- pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/util/HTMLSubSupConverter.java
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/util/HTMLSubSupConverter.java
@@ -1,5 +1,8 @@
 package de.mpg.escidoc.pubman.util;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.*;
@@ -7,6 +10,12 @@ import de.mpg.escidoc.services.citationmanager.utils.Utils;
 
 public class HTMLSubSupConverter implements Converter{
     public static final String CONVERTER_ID = "HTMLSubSupConverter";
+    
+	private static final String startSub = "<sub>";
+	private static final String endSub = "</sub>";
+	private static final String startSup = "<sup>";
+	private static final String endSup = "</sup>";
+	
 	public HTMLSubSupConverter()
 	{
 		
@@ -16,14 +25,56 @@ public class HTMLSubSupConverter implements Converter{
 	{
 		return null;
 	}
-
+	
 	public String getAsString(FacesContext arg0, UIComponent arg1, Object object) 
 	{
         String snippet = (String) object;
 		snippet = Utils.replaceAllTotal(snippet, "\\&(?!amp;)", "&amp;");
-		snippet = Utils.replaceAllTotal(snippet, "\\<(?!(\\/?style)|(\\/?su[bp]))", "&lt;");
-		snippet = Utils.replaceAllTotal(snippet, "\\<((\\/?su[bp]))\\>", "");
+		if(isBalanced(snippet, startSub, endSub, startSup, endSup))
+		{
+			snippet = Utils.replaceAllTotal(snippet, "\\<(?!(\\/?style)|(\\/?su[bp]))", "&lt;");
+		}
+		else
+		{
+			snippet = Utils.replaceAllTotal(snippet, "\\<(?!(\\/?style))", "&lt;");
+		}
+//		snippet = Utils.replaceAllTotal(snippet, "\\<((\\/?su[bp]))\\>", "");
 		return snippet;
 	}
+	
+	public static boolean isBalanced(String snippet, String start, String end, String start2, String end2) {
+		Stack s = new Stack(); // Create an empty stack.
+		boolean balanced = true;
+		try {
+			if(balanced){
+				for (int index=0; index < snippet.length(); index++) 
+				{
+					if (index+start.length()<=snippet.length() && (start.equals(snippet.substring(index,index+start.length())) || start2.equals(snippet.substring(index,index+start.length()))) )
+						{
+							s.push(snippet.substring(index, index+start.length()));
+							index += start.length()-1;
+						} 
+					
+					if (index +end.length()<=snippet.length() && (end.equals(snippet.substring(index, index+end.length())) || end2.equals(snippet.substring(index, index+end.length()))) )
+						{
+							if(s.lastElement().toString().substring(1, 4).equals(snippet.substring(index+2, index+end.length()-1)))
+							{
+								s.pop();
+								index += end.length()-1;
+							}
+							else
+								balanced = false;
+						}
+				}
+			}
+		} 
+		catch (EmptyStackException ex) {
+			balanced = false;
+		}
+		return (balanced && s.empty());
+	}
+
+
 
 }
+ 
