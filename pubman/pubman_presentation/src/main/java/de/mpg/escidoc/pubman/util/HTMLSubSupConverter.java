4cd06417c2f5adefd0b4cb3968dfe91a1fc40ffe
Markus Haarlaender <haarlaender@mpdl.mpg.de>
2010-12-02 16:20:32 +0000

    Former-commit-id: a4378d7f741544cd028bbca27e69de93a05a2237

--- pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/util/HTMLSubSupConverter.java
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/util/HTMLSubSupConverter.java
@@ -1,6 +1,8 @@
 package de.mpg.escidoc.pubman.util;
 
+import java.util.ArrayList;
 import java.util.EmptyStackException;
+import java.util.List;
 import java.util.Stack;
 
 import javax.faces.component.UIComponent;
@@ -27,7 +29,7 @@ public class HTMLSubSupConverter implements Converter{
 		snippet = Utils.replaceAllTotal(snippet, "\\&(?!amp;)", "&amp;");
 		if(checkSubSupTag(snippet))
 		{
-			snippet = Utils.replaceAllTotal(snippet, "\\<(?!|(\\/?su[bp]))", "&lt;");
+			snippet = Utils.replaceAllTotal(snippet, "\\<(?!(\\/?su[bp]))", "&lt;");
 		}
 		else
 		{
@@ -73,6 +75,149 @@ public class HTMLSubSupConverter implements Converter{
 			balanced = false;
 		}
 		return (balanced && s.empty());
+	}
+	
+	public boolean checkTag(String tag)
+	{
+		
+	
+		String snippet="Test Bo&ok about H<sub>2</sub>O with <b>Kap1</b> <i>italic</i>111";
+		
+		if(snippet.matches("\\<sub\\>|\\<sup\\>"))
+		{
+			int charnumber = 0;
+			List<Integer> openings = new ArrayList<Integer>();
+			List<Integer> closing = new ArrayList<Integer>();
+			
+			int openingmatchPos = 0;
+			while  (openingmatchPos!=-1)
+			{
+				openingmatchPos = snippet.indexOf("<"+ tag +">");
+				if(openingmatchPos!=-1)
+				{
+					openings.add(openingmatchPos);
+				}
+			}
+			
+			int closingmatchPos = 0;
+			while  (closingmatchPos!=-1)
+			{
+				closingmatchPos = snippet.indexOf("</" + tag + ">");
+				if(closingmatchPos!=-1)
+				{
+					closing.add(closingmatchPos);
+				}
+			}
+	
+			if(openings.size()==closing.size())
+			{
+				
+				for (int i=0; i<openings.size(); i++) {
+					if(openings.get(i)>closing.get(i))
+					{
+						return false;
+					}
+				}
+			}
+			else
+			{
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		String snippet="Test Bo&ok about H<sub>2</sub>O with <b>Kap1</b> <i>italic</i>111";
+		
+		if(snippet.matches("\\<sub\\>|\\<sup\\>"))
+		{
+			int charnumber = 0;
+			List<Integer> openings = new ArrayList<Integer>();
+			List<Integer> closing = new ArrayList<Integer>();
+			
+			int openingmatchPos = 0;
+			while  (openingmatchPos!=-1)
+			{
+				openingmatchPos = snippet.indexOf("<sub>");
+				if(openingmatchPos!=-1)
+				{
+					openings.add(openingmatchPos);
+				}
+			}
+			
+			int closingmatchPos = 0;
+			while  (closingmatchPos!=-1)
+			{
+				closingmatchPos = snippet.indexOf("</sub>");
+				if(closingmatchPos!=-1)
+				{
+					closing.add(closingmatchPos);
+				}
+			}
+
+			if(openings.size()==closing.size())
+			{
+				
+				for (int i=0; i<openings.size(); i++) {
+					if(openings.get(i)<closing.get(i))
+					{
+						
+					}
+				}
+			}
+		}
+		
+		
+		/*
+		snippet = snippet.replaceAll("<", "&lt;");
+		StringReader reader = new StringReader(snippet);
+		String substring;
+		int c = 0;
+        int opened=0;
+        int closed=0;
+        String tag ="";
+        
+		while ((c = reader.read()) != -1)
+        {
+            substring += (char)c;
+            if (substring.matches("\\&lt;" + tag + ">"))
+    		{
+            	opened++;
+            	substring="";
+            	
+    		}
+            if (substring.matches("\\&lt;\\/" + tag + ">"))
+    		{
+            	closed++;
+            	substring="";
+    		}
+    	
+        }
+		*/
+		//snippet = Utils.replaceAllTotal(snippet, "\\<", "&lt;");
+		//snippet = Utils.replaceAllTotal(snippet, "((?<=\\&lt;sub\\>)[^\\&lt;sub\\>]*?(?=\\&lt;\\/sub\\>))", "<sub>$1</sub>");
+		
+		//Pattern p = Pattern.compile("(\\&lt;sub\\>)^(.*\\&lt;sub\\>.*)(\\&lt;\\/sub\\>)", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+		
+		/*
+		Pattern p = Pattern.compile("\\&lt;((?=sub\\>.*(?![&lt;|\\<]sub>).*?\\&lt;\\/sub\\>){1})", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+		 Matcher m = p.matcher(snippet);
+		 snippet =  m.replaceAll("<");
+		 */
+	     //snippet=snippet.replaceAll("\\&lt;\\/sub>", "</sub>")
+		 
+		 //snippet =  m.replaceAll("<");
+		 
+		
+		 //System.out.println(snippet);
+
+		//snippet = Utils.replaceAllTotal(snippet, "(?=\\<sub\\>.*?)\\<(?=\\/sub\\>)", "XX");
+		
+		//System.out.println(snippet);
 	}
 
 
