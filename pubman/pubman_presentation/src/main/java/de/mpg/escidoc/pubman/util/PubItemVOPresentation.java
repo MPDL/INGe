3fb3a2a9f9ccc291316cc8aff7ab4db59127c7f1
Lu Yu <yu@mpdl.mpg.de>
2011-01-05 14:12:39 +0000

    Former-commit-id: a6522eaaa8abeb11c2d1b1dbde5c975c92891cea

--- pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/util/PubItemVOPresentation.java
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/util/PubItemVOPresentation.java
@@ -230,7 +230,8 @@ public class PubItemVOPresentation extends PubItemVO implements Internationalize
             WrappedLocalTag wrappedLocalTag = new WrappedLocalTag();
             wrappedLocalTag.setParent(this);
             wrappedLocalTag.setValue(this.getLocalTags().get(i));
-            wrappedLocalTags.add(wrappedLocalTag);
+            if(wrappedLocalTag.getValue().length()>0)
+            	wrappedLocalTags.add(wrappedLocalTag);
         }
         
     }
