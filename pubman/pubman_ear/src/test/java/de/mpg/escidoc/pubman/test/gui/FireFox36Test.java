31e7e2f968e2bace132f15c798cb17433c8f8841
Lu Yu <yu@mpdl.mpg.de>
2011-01-31 16:32:51 +0000

    firefox 3,6 selenium test, easy submission(genre=other), full submission(genre=paper)
    Former-commit-id: 62a05f680062a90704745b747f7a798ede8e3705

---
+++ pubman/pubman_ear/src/test/java/de/mpg/escidoc/pubman/test/gui/FireFox36Test.java
@@ -0,0 +1,29 @@
+package de.mpg.escidoc.pubman.test.gui;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+
+public class FireFox36Test extends PubmanGuiTestcase
+{
+
+	public FireFox36Test() throws IOException {
+		super();
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+//		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://dev-pubman.mpdl.mpg.de/");
+		selenium = new DefaultSelenium("localhost", 4444, "*firefox3 C:/Program Files (x86)/Mozilla Firefox/firefox.exe", "http://localhost:8080/");
+		selenium.start();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+
+}
