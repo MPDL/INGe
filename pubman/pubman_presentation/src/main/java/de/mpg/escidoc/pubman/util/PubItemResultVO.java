29c56afe5a2253230e2e4d56bdcdf261849f7786
Natasa Bulatovic <bulatovic@mpdl.mpg.de>
2011-03-14 17:21:28 +0000

    Performance improvement
    Former-commit-id: a4d264f47ba3aa767d7e74e61d3cf1837f10e5b7

--- pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/util/PubItemResultVO.java
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/util/PubItemResultVO.java
@@ -15,40 +15,45 @@ import de.mpg.escidoc.services.common.valueobjects.publication.PubItemVO;
  */
 public class PubItemResultVO extends PubItemVO {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
-    
-    /**
-     * List of hits. Every hit in files contains the file reference and the text fragments of the search hit.
-     */
-    private java.util.List<SearchHitVO> searchHitList = new java.util.ArrayList<SearchHitVO>();
-    
-    private float score;
-    
-    /**
-     * Delivers the list of search hits.
-     */
-    public List<SearchHitVO> getSearchHitList()
-    {
-        return searchHitList;
-    }
-    
-    public PubItemResultVO(ItemVO itemVO, List<SearchHitVO> searchHits, float score )
-    {
-        super(itemVO);
-        this.searchHitList = searchHits;
-        this.score = score;
-    }
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    public void setScore(float score)
-    {
-        this.score = score;
-    }
+	/**
+	 * List of hits. Every hit in files contains the file reference and the text fragments of the search hit.
+	 */
+	private java.util.List<SearchHitVO> searchHitList;
+	//= new java.util.ArrayList<SearchHitVO>();
 
-    public float getScore()
-    {
-        return score;
-    }
+	private float score;
+
+	/**
+	 * Delivers the list of search hits.
+	 */
+	public List<SearchHitVO> getSearchHitList()
+	{
+		return searchHitList;
+	}
+
+	public PubItemResultVO(ItemVO itemVO, List<SearchHitVO> searchHits, float score )
+	{
+		super(itemVO);
+		if (searchHits.size()>0)
+		{
+			this.searchHitList = new java.util.ArrayList<SearchHitVO>();
+			this.searchHitList = searchHits;
+			this.score = score;
+		}
+	}
+
+	public void setScore(float score)
+	{
+		this.score = score;
+	}
+
+	public float getScore()
+	{
+		return score;
+	}
 }
