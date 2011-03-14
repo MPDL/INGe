ced93b5c1978afc408e5f15e7cdb5e589b21e92b
Natasa Bulatovic <bulatovic@mpdl.mpg.de>
2011-03-14 15:32:34 +0000

    Added Login during init (called from superclass method)
    Former-commit-id: 556ed09c757c9f91914840d27c052c3d96261533

--- pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/EasySubmissionPage.java
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/EasySubmissionPage.java
@@ -1,32 +1,32 @@
 /*
-*
-* CDDL HEADER START
-*
-* The contents of this file are subject to the terms of the
-* Common Development and Distribution License, Version 1.0 only
-* (the "License"). You may not use this file except in compliance
-* with the License.
-*
-* You can obtain a copy of the license at license/ESCIDOC.LICENSE
-* or http://www.escidoc.de/license.
-* See the License for the specific language governing permissions
-* and limitations under the License.
-*
-* When distributing Covered Code, include this CDDL HEADER in each
-* file and include the License file at license/ESCIDOC.LICENSE.
-* If applicable, add the following below this CDDL HEADER, with the
-* fields enclosed by brackets "[]" replaced with your own identifying
-* information: Portions Copyright [yyyy] [name of copyright owner]
-*
-* CDDL HEADER END
-*/
+ *
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License"). You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at license/ESCIDOC.LICENSE
+ * or http://www.escidoc.de/license.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at license/ESCIDOC.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
 
 /*
-* Copyright 2006-2011 Fachinformationszentrum Karlsruhe Gesellschaft
-* für wissenschaftlich-technische Information mbH and Max-Planck-
-* Gesellschaft zur Förderung der Wissenschaft e.V.
-* All rights reserved. Use is subject to license terms.
-*/
+ * Copyright 2006-2011 Fachinformationszentrum Karlsruhe Gesellschaft
+ * für wissenschaftlich-technische Information mbH and Max-Planck-
+ * Gesellschaft zur Förderung der Wissenschaft e.V.
+ * All rights reserved. Use is subject to license terms.
+ */
 
 package de.mpg.escidoc.pubman;
 
@@ -47,37 +47,40 @@ import de.mpg.escidoc.pubman.easySubmission.EasySubmission;
  */
 public class EasySubmissionPage extends BreadcrumbPage
 {
-    private static Logger logger = Logger.getLogger(EasySubmissionPage.class);
-    
-    public static final String BEAN_NAME = "EasySubmissionPage";
-    
-    /**
-     * Public constructor.
-     */
-    public EasySubmissionPage()
-    {
-        this.init();
-    }
+	private static Logger logger = Logger.getLogger(EasySubmissionPage.class);
 
-    /**
-     * Callback method that is called whenever a page containing this page fragment is navigated to, either directly via
-     * a URL, or indirectly via page navigation.
-     */
-    public void init()
-    {
-        // Perform initializations inherited from our superclass
-        super.init();
-    }
+	public static final String BEAN_NAME = "EasySubmissionPage";
 
-    protected Method getDefaultAction() throws NoSuchMethodException
-    {
-        EasySubmission easySubmission = (EasySubmission) getRequestBean(EasySubmission.class);
-        return easySubmission.getClass().getMethod("newEasySubmission", null);
-    }
+	/**
+	 * Public constructor.
+	 */
+	public EasySubmissionPage()
+	{
+		this.init();
+	}
 
-    @Override
-    public boolean isItemSpecific()
-    {
-        return true;
-    }
+	/**
+	 * Callback method that is called whenever a page containing this page fragment is navigated to, either directly via
+	 * a URL, or indirectly via page navigation.
+	 */
+	@Override
+	public void init()
+	{
+		// Perform initializations inherited from our superclass
+		super.init();
+		checkForLogin();
+	}
+
+	@Override
+	protected Method getDefaultAction() throws NoSuchMethodException
+	{
+		EasySubmission easySubmission = (EasySubmission) getRequestBean(EasySubmission.class);
+		return easySubmission.getClass().getMethod("newEasySubmission", null);
+	}
+
+	@Override
+	public boolean isItemSpecific()
+	{
+		return true;
+	}
 }
