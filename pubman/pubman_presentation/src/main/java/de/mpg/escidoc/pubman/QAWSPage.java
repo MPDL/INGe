efe045f2ab387c56f20c6e5bea7ed1ef67d18a6e
Natasa Bulatovic <bulatovic@mpdl.mpg.de>
2011-03-15 09:42:13 +0000

    Safer handling in accordance with privileges
    Former-commit-id: 802835c522bf195fcfd4f94f7b58c646cc8db2a8

--- pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/QAWSPage.java
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/QAWSPage.java
@@ -1,37 +1,34 @@
 /*
-*
-* CDDL HEADER START
-*
-* The contents of this file are subject to the terms of the
-* Common Development and Distribution License, Version 1.0 only
-* (the "License"). You may not use this file except in compliance
-* with the License.
-*
-* You can obtain a copy of the license at license/ESCIDOC.LICENSE
-* or http://www.escidoc.de/license.
-* See the License for the specific language governing permissions
-* and limitations under the License.
-*
-* When distributing Covered Code, include this CDDL HEADER in each
-* file and include the License file at license/ESCIDOC.LICENSE.
-* If applicable, add the following below this CDDL HEADER, with the
-* fields enclosed by brackets "[]" replaced with your own identifying
-* information: Portions Copyright [yyyy] [name of copyright owner]
-*
-* CDDL HEADER END
-*/
+ *
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License"). You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at license/ESCIDOC.LICENSE
+ * or http://www.escidoc.de/license.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at license/ESCIDOC.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
 
 /*
-* Copyright 2006-2011 Fachinformationszentrum Karlsruhe Gesellschaft
-* für wissenschaftlich-technische Information mbH and Max-Planck-
-* Gesellschaft zur Förderung der Wissenschaft e.V.
-* All rights reserved. Use is subject to license terms.
-*/ 
+ * Copyright 2006-2011 Fachinformationszentrum Karlsruhe Gesellschaft
+ * für wissenschaftlich-technische Information mbH and Max-Planck-
+ * Gesellschaft zur Förderung der Wissenschaft e.V.
+ * All rights reserved. Use is subject to license terms.
+ */
 
 package de.mpg.escidoc.pubman;
-
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 
@@ -39,7 +36,7 @@ import de.mpg.escidoc.pubman.appbase.BreadcrumbPage;
 import de.mpg.escidoc.pubman.util.LoginHelper;
 import de.mpg.escidoc.pubman.viewItem.ViewItemSessionBean;
 
- 
+
 /**
  * BackingBean for Quality Assurance Page (QAWSPage.jsp).
  *
@@ -50,42 +47,43 @@ import de.mpg.escidoc.pubman.viewItem.ViewItemSessionBean;
  */
 public class QAWSPage extends BreadcrumbPage
 {
-    private static Logger logger = Logger.getLogger(QAWSPage.class);
-    public static final String BEAN_NAME = "QAWSPage";
-    
-    
-    
-    /**
-     * Public constructor.
-     */
-    public QAWSPage()
-    {
-        this.init();
-    }
+	private static Logger logger = Logger.getLogger(QAWSPage.class);
+	public static final String BEAN_NAME = "QAWSPage";
 
-    /**
-     * Callback method that is called whenever a page containing this page fragment is navigated to, either directly via
-     * a URL, or indirectly via page navigation. 
-     */
-    public void init()
-    {
-        super.init();
-        
-        FacesContext fc = FacesContext.getCurrentInstance();
+
+
+	/**
+	 * Public constructor.
+	 */
+	public QAWSPage()
+	{
+		this.init();
+	}
+
+	/**
+	 * Callback method that is called whenever a page containing this page fragment is navigated to, either directly via
+	 * a URL, or indirectly via page navigation.
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
+		checkForLogin();
+		/*FacesContext fc = FacesContext.getCurrentInstance();
         HttpServletRequest request = (HttpServletRequest) fc.getExternalContext().getRequest();
         String userHandle = request.getParameter(LoginHelper.PARAMETERNAME_USERHANDLE);
 
         if (logger.isDebugEnabled())
         {
             logger.debug("UserHandle: " + userHandle);
-        }        
-        
+        }
+
         LoginHelper loginHelper = (LoginHelper) getSessionBean(LoginHelper.class);
         if(loginHelper == null)
         {
             loginHelper = new LoginHelper();
         }
-        
+
         try
         {
             loginHelper.checkLogin();
@@ -93,45 +91,41 @@ public class QAWSPage extends BreadcrumbPage
         catch (Exception e)
         {
             logger.error("Could not login." + "\n" + e.toString());
-        }
-        
-        this.getViewItemSessionBean().setHasBeenRedirected(true);
-        
-        
-    }
+        }*/
 
-    
-   
-    
-    /**
-     * Returns the ViewItemSessionBean.
-     * 
-     * @return a reference to the scoped data bean (ViewItemSessionBean)
-     */
-    protected ViewItemSessionBean getViewItemSessionBean()
-    {
-        return (ViewItemSessionBean)getBean(ViewItemSessionBean.class);
-    }
-    
-    public boolean getIsModerator()
-    {
-        LoginHelper loginHelper = (LoginHelper) getSessionBean(LoginHelper.class);
-        boolean isModerator = false;
-        
-        if (loginHelper.isLoggedIn()) 
-        {
-            isModerator = loginHelper.getAccountUser().isModerator();
-        }
-        
-        return isModerator;
-        
-            
-    }
+		this.getViewItemSessionBean().setHasBeenRedirected(true);
 
-    @Override
-    public boolean isItemSpecific() 
-    {
-        return false;
-    }
+	}
+
+	/**
+	 * Returns the ViewItemSessionBean.
+	 * 
+	 * @return a reference to the scoped data bean (ViewItemSessionBean)
+	 */
+	protected ViewItemSessionBean getViewItemSessionBean()
+	{
+		return (ViewItemSessionBean)getBean(ViewItemSessionBean.class);
+	}
+
+	public boolean getIsModerator()
+	{
+		LoginHelper loginHelper = (LoginHelper) getSessionBean(LoginHelper.class);
+		boolean isModerator = false;
+
+		if (loginHelper.isLoggedIn())
+		{
+			isModerator = loginHelper.getAccountUser().isModerator();
+		}
+
+		return isModerator;
+
+
+	}
+
+	@Override
+	public boolean isItemSpecific()
+	{
+		return false;
+	}
 
 }
