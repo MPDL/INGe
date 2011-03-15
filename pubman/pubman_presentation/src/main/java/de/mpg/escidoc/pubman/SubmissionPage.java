87f484a937283ceeb41910513c68429b2af691a1
Natasa Bulatovic <bulatovic@mpdl.mpg.de>
2011-03-15 09:41:31 +0000

    Safer handling in accordance with privileges
    Former-commit-id: 3545111b301ab05f2db51ed1f82e735d6e86edf1

--- pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/SubmissionPage.java
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/SubmissionPage.java
@@ -1,31 +1,31 @@
 /*
-* CDDL HEADER START
-*
-* The contents of this file are subject to the terms of the
-* Common Development and Distribution License, Version 1.0 only
-* (the "License"). You may not use this file except in compliance
-* with the License.
-*
-* You can obtain a copy of the license at license/ESCIDOC.LICENSE
-* or http://www.escidoc.de/license.
-* See the License for the specific language governing permissions
-* and limitations under the License.
-*
-* When distributing Covered Code, include this CDDL HEADER in each
-* file and include the License file at license/ESCIDOC.LICENSE.
-* If applicable, add the following below this CDDL HEADER, with the
-* fields enclosed by brackets "[]" replaced with your own identifying
-* information: Portions Copyright [yyyy] [name of copyright owner]
-*
-* CDDL HEADER END
-*/
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License"). You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at license/ESCIDOC.LICENSE
+ * or http://www.escidoc.de/license.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at license/ESCIDOC.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
 
 /*
-* Copyright 2006-2011 Fachinformationszentrum Karlsruhe Gesellschaft
-* für wissenschaftlich-technische Information mbH and Max-Planck-
-* Gesellschaft zur Förderung der Wissenschaft e.V.
-* All rights reserved. Use is subject to license terms.
-*/
+ * Copyright 2006-2011 Fachinformationszentrum Karlsruhe Gesellschaft
+ * für wissenschaftlich-technische Information mbH and Max-Planck-
+ * Gesellschaft zur Förderung der Wissenschaft e.V.
+ * All rights reserved. Use is subject to license terms.
+ */
 
 package de.mpg.escidoc.pubman;
 
@@ -44,32 +44,34 @@ import de.mpg.escidoc.pubman.appbase.BreadcrumbPage;
  */
 public class SubmissionPage extends BreadcrumbPage
 {
-    @SuppressWarnings("unused")
-    private static Logger logger = Logger.getLogger(SubmissionPage.class);
+	@SuppressWarnings("unused")
+	private static Logger logger = Logger.getLogger(SubmissionPage.class);
 
-    /**
-     * Public constructor
-     */
-    public SubmissionPage()
-    {
-        this.init();
-    }
+	/**
+	 * Public constructor
+	 */
+	public SubmissionPage()
+	{
+		this.init();
+	}
 
-    /**
-     * Callback method that is called whenever a page is navigated to, either directly via a URL, or indirectly via page
-     * navigation.
-     */
-    public void init()
-    {
-        // Perform initializations inherited from our superclass
-        super.init();
- 
-    }
+	/**
+	 * Callback method that is called whenever a page is navigated to, either directly via a URL, or indirectly via page
+	 * navigation.
+	 */
+	@Override
+	public void init()
+	{
+		// Perform initializations inherited from our superclass
+		super.init();
+		checkForLogin();
 
-    @Override
-    public boolean isItemSpecific() 
-    {
-        return false;
-    }
+	}
+
+	@Override
+	public boolean isItemSpecific()
+	{
+		return false;
+	}
 
 }
