d33d637527329936b04e2220ace0fa163271e092
Markus Haarlaender <haarlaender@mpdl.mpg.de>
2013-02-21 16:19:58 +0000

    PUBMAN-1950 fixed: Wrong isModerator method logic

--- common_services/common_logic/src/main/java/de/mpg/escidoc/services/common/valueobjects/intelligent/grants/Grant.java
+++ common_services/common_logic/src/main/java/de/mpg/escidoc/services/common/valueobjects/intelligent/grants/Grant.java
@@ -424,7 +424,7 @@ public class Grant extends IntelligentVO
             throw new IllegalArgumentException(getClass().getSimpleName()
                     + ":isModerator:objectRef is null");
         }
-        return (CoreserviceRole.DEPOSITOR.getRoleId().equals(role) && this.assignedOn.equals(objRef.getObjectId()));
+        return (CoreserviceRole.MODERATOR.getRoleId().equals(role) && this.assignedOn.equals(objRef.getObjectId()));
     }
 
     
