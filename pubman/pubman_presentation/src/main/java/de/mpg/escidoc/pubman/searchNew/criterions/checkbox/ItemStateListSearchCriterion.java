c9aa8e71f33255d5b35e62d4db4373f36cba814d
Markus Haarlaender <haarlaender@mpdl.mpg.de>
2014-01-28 10:51:55 +0000

    Preparations for PUBMAN-1523 :Implementation of Administrative Search (PL)
    Former-commit-id: 6936124511e3e127f059af287ce6b42347b67955

---
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/searchNew/criterions/checkbox/ItemStateListSearchCriterion.java
@@ -0,0 +1,61 @@
+package de.mpg.escidoc.pubman.searchNew.criterions.checkbox;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import de.mpg.escidoc.pubman.searchNew.SearchParseException;
+import de.mpg.escidoc.pubman.searchNew.criterions.SearchCriterionBase;
+import de.mpg.escidoc.pubman.searchNew.criterions.component.MapListSearchCriterion;
+import de.mpg.escidoc.pubman.searchNew.criterions.standard.StandardSearchCriterion;
+import de.mpg.escidoc.services.common.valueobjects.FileVO.Visibility;
+
+public class ItemStateListSearchCriterion extends MapListSearchCriterion {
+
+	public ItemStateListSearchCriterion() {
+		
+		super(getItemStateMap());
+		// TODO Auto-generated constructor stub
+	}
+	
+	private static Map<String, String> getItemStateMap()
+	{
+		Map<String, String> itemStateMap = new LinkedHashMap<String, String>();
+
+		itemStateMap.put("PENDING", "pending");
+		itemStateMap.put("SUBMITTED", "submitted");
+		itemStateMap.put("IN_REVISION", "in-revision");
+		itemStateMap.put("RELEASED", "released");
+		itemStateMap.put("WITHDRAWN", "withdrawn");
+		
+		
+		return itemStateMap;
+	}
+
+	@Override
+	public String[] getCqlIndexes(Index indexName, String value) {
+		switch(indexName)
+		{
+			case ITEM_CONTAINER_ADMIN : 
+				{
+					if("withdrawn".equals(value))
+					{
+						return new String[] {"\"/properties/public-status\""}; 
+					}
+					else
+					{
+						return new String[] {"\"/properties/version/status\""};
+						
+					}
+				}
+		}
+		
+		return null;
+	}
+
+	
+
+	
+	
+
+}
