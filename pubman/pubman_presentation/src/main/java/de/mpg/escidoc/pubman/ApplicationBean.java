a8a191f1a1d553f6626f6fc69f02150de17efcae
Markus Haarlaender <haarlaender@mpdl.mpg.de>
2014-06-18 13:59:06 +0000

    Former-commit-id: 05fd1b694a48115b79915dffcf7336f7a25cb34d

--- pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/ApplicationBean.java
+++ pubman/pubman_presentation/src/main/java/de/mpg/escidoc/pubman/ApplicationBean.java
@@ -395,7 +395,6 @@ public class ApplicationBean extends FacesBean
         {
             throw new PubManStylesheetNotAvailableException(e);
         }
-        System.out.println(styleTags.toString());
         return styleTags.toString();
         
     }
