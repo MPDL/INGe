1d19b1a6daea93be68a84b25926a1baa1ec99003
Markus Haarlaender <haarlaender@mpdl.mpg.de>
2014-09-02 14:11:40 +0000

    PUBMAN-2062 fixed: User logout does not work properly in CoNE

---
+++ common_services/aa_presentation/src/main/java/de/mpg/escidoc/services/aa/web/client/LogoutClient.java
@@ -0,0 +1,46 @@
+package de.mpg.escidoc.services.aa.web.client;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+public class LogoutClient extends Client {
+
+	private static final Logger logger = Logger.getLogger(LogoutClient.class);
+	
+	@Override
+	protected void process(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		request.getSession().removeAttribute("authentication");
+		
+		try
+        {
+            response.sendRedirect(getLogoutUrl(request, response));
+        }
+        catch (IllegalStateException ise)
+        {
+            logger.warn("Caught IllegalStateException: DEBUG for more info");
+            logger.debug("LogoutClient tried to send a redirect, but there was probably already a header defined.");
+        }
+		
+
+	}
+	
+	protected String getLogoutUrl(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		String target = request.getParameter("target");
+		
+		if(target != null)
+		{
+			return target;
+		}
+		else
+		{
+			logger.warn("No query parameter 'target' found for logging out.");
+			return null;
+		}
+	}
+
+}
