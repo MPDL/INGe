7598018ea3c383de1fbe2b832d89f6bbb688e511
Gudrun Siedersleben <siedersleben@mpdl.mpg.de>
2014-10-20 09:09:26 +0000

    commented source removed
    Former-commit-id: d0361d02d2a057c12a33b00c4ba7d93083b0fccb

--- pubman/pubman_installer/src/main/java/de/mpg/escidoc/pubman/installer/ConeInsertProcess.java
+++ pubman/pubman_installer/src/main/java/de/mpg/escidoc/pubman/installer/ConeInsertProcess.java
@@ -94,22 +94,9 @@ public class ConeInsertProcess extends Thread
 		   panel.getTextArea().append("Good. CoNE data inserted.\n");
 		   
 		   panel.getTextArea().append("Processing Cone Data...\n");
-		   
-		   //PropertyReader.
-		   /*
-		   logger.info("Processing CoNe database.");
-		   coneDataset.processConeData();
-		   logger.info("CoNE processing finished!");
-			*/
 		   panel.getTextArea().append("\n\n\n");
 		   panel.getTextArea().append("DONE. You can proceed with 'Next' now.\n");
-		   /*File pf = new File(idata.getInstallPath() + coneInsertDataFile);
-		   File dir = pf.getParentFile();
-		   if (dir == null || !dir.exists())
-		   {
-		       Configuration.createDir(pf.getAbsolutePath().substring(0, pf.getAbsolutePath().lastIndexOf("/")));
-		   }
-		   pf.createNewFile();*/
+		   
 		   logger.info("Cone initialization finished successfully");
 		
 		
