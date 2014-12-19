c562f4a5ac370df331a2613d9fc2c771de351820
Markus Haarlaender <haarlaender@mpdl.mpg.de>
2014-12-19 11:03:35 +0000

    Added new metadata schema project-info and new cleanup functionality

---
+++ common_services/common_logic/src/main/java/de/mpg/escidoc/services/common/valueobjects/IgnoreForCleanup.java
@@ -0,0 +1,12 @@
+package de.mpg.escidoc.services.common.valueobjects;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface IgnoreForCleanup {
+
+}
