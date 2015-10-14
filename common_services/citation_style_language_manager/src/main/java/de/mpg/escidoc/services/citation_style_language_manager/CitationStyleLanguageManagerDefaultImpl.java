06cf51d2a039b870343c3365a782a2727fe9b457
walterMPDL <walter@mpdl.mpg.de>
2015-10-14 10:07:31 +0200

    CitationStyleLanguageManagerDefaultImpl changed

--- common_services/citation_style_language_manager/src/main/java/de/mpg/escidoc/services/citation_style_language_manager/CitationStyleLanguageManagerDefaultImpl.java
+++ common_services/citation_style_language_manager/src/main/java/de/mpg/escidoc/services/citation_style_language_manager/CitationStyleLanguageManagerDefaultImpl.java
@@ -38,152 +38,145 @@ import de.undercouch.citeproc.output.Bibliography;
 import de.mpg.escidoc.services.common.valueobjects.ExportFormatVO;
 
 /**
- * CitationStyleLanguageManagerDefaultImpl is used to generate a citation for an
- * escidoc item or a list of escidoc items
+ * CitationStyleLanguageManagerDefaultImpl is used to generate a citation for an escidoc item or a list of escidoc items
  * 
  * @author walter
- * 
  */
 @Stateless
 @Remote
-public class CitationStyleLanguageManagerDefaultImpl implements
-		CitationStyleLanguageManagerInterface {
+public class CitationStyleLanguageManagerDefaultImpl implements CitationStyleLanguageManagerInterface
+{
+    private final static Logger logger = Logger.getLogger(CitationStyleLanguageManagerDefaultImpl.class);
+    private final static String TRANSFORMATION_ITEM_LIST_2_SNIPPET = "itemList2snippet.xsl";
+    private final static String CITATION_EXTRACTION_PATTERN = "<div[^>]*>[^\\[](.*?)</div>";
 
-	private final static Logger logger = Logger
-			.getLogger(CitationStyleLanguageManagerDefaultImpl.class);
-	
-	private final static String TRANSFORMATION_ITEM_LIST_2_SNIPPET = "itemList2snippet.xsl";
-	private final static String CITATION_EXTRACTION_PATTERN = "<div[^>]*>[^\\[](.*?)</div>";
+    /*
+     * (non-Javadoc)
+     * @see org.citation_style_language_manager.CitationStyleLanguageManagerInterface #getOutput(java.lang.String,
+     * java.lang.String, java.lang.String)
+     */
+    @Override
+    public byte[] getOutput(ExportFormatVO exportFormat, String itemList)
+    {
+        List<String> citationList = new ArrayList<String>();
+        StringWriter snippet = new StringWriter();
+        byte[] result = null;
+        try
+        {
+            ItemDataProvider itemDataProvider = new MetadataProvider(itemList);
+            CSL citeproc = new CSL(itemDataProvider,
+                    CitationStyleLanguageUtils.loadStyleFromJsonUrl(exportFormat.getId()));
+            citeproc.registerCitationItems(itemDataProvider.getIds());
+            citeproc.setOutputFormat("html");
+            Bibliography bibl = citeproc.makeBibliography();
+            TransformerFactory factory = new TransformerFactoryImpl();
+            Transformer transformer = factory.newTransformer(new StreamSource(
+                    this.getClass().getClassLoader().getResourceAsStream(TRANSFORMATION_ITEM_LIST_2_SNIPPET)));
+            // Create a Pattern object
+            Pattern pattern = Pattern.compile(CITATION_EXTRACTION_PATTERN);
+            // Now create matcher object.
+            for (String citation : bibl.getEntries())
+            {
+                if (citation.contains("<div class=\"csl-right-inline\">"))
+                {
+                    citation = citation.substring(citation.indexOf("<div class=\"csl-right-inline\">") + 30);
+                    citation = citation.substring(0, citation.indexOf("</div>"));
+                }
+                else if (citation.contains("<div class=\"csl-entry\">"))
+                {
+                    citation = citation.substring(citation.indexOf("<div class=\"csl-entry\">") + 23);
+                    citation = citation.substring(0, citation.indexOf("</div>"));
+                }
+                else
+                {
+                    citation = citation.trim();
+                }
+                if (logger.isDebugEnabled())
+                {
+                    logger.debug("Citation: " + citation);
+                }
+                citationList.add(citation);
+            }
+            transformer.setParameter("citations", citationList);
+            transformer.transform(new StreamSource(new StringReader(itemList)), new StreamResult(snippet));
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("eSciDoc-Snippet including Ciation: " + snippet);
+            }
+            result = snippet.toString().getBytes("UTF-8");
+        }
+        catch (IOException e)
+        {
+            logger.error("Error creating CSL processor", e);
+        }
+        catch (TransformerConfigurationException e)
+        {
+            logger.error("Eror preparing transformation itemList to snippet", e);
+        }
+        catch (TransformerException e)
+        {
+            logger.error("Eror transforming itemList to snippet", e);
+        }
+        return result;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.citation_style_language_manager.CitationStyleLanguageManagerInterface
-	 * #getOutput(java.lang.String, java.lang.String, java.lang.String)
-	 */
-	@Override
-	public byte[] getOutput(ExportFormatVO exportFormat,
-			String itemList) {
-		List<String> citationList = new ArrayList<String>();
-		StringWriter snippet = new StringWriter();
-		byte[] result = null;
-		try {
-			ItemDataProvider itemDataProvider = new MetadataProvider(itemList);
-			CSL citeproc = new CSL(
-					itemDataProvider,
-					CitationStyleLanguageUtils.loadStyleFromJsonUrl(exportFormat.getId()));
-			citeproc.registerCitationItems(itemDataProvider.getIds());
-			citeproc.setOutputFormat("html");
-			Bibliography bibl = citeproc.makeBibliography();
-			TransformerFactory factory = new TransformerFactoryImpl();
-			Transformer transformer = factory.newTransformer(new StreamSource(this.getClass().getClassLoader().getResourceAsStream(TRANSFORMATION_ITEM_LIST_2_SNIPPET)));
-			
+    /*
+     * (non-Javadoc)
+     * @see org.citation_style_language_manager.CitationStyleLanguageManagerInterface #isCitationStyle(java.lang.String)
+     */
+    @Override
+    public boolean isCitationStyle(String cs)
+    {
+        return false;
+    }
 
-	        // Create a Pattern object
-	        Pattern pattern = Pattern.compile(CITATION_EXTRACTION_PATTERN);
-
-	        // Now create matcher object.
-	        Matcher matcher = pattern.matcher("");
-			for (String citation : bibl.getEntries()) {
-//				matcher.reset(citation);
-//				if (matcher.find())
-//				{
-//					citation = matcher.group(0);
-//				}
-				if (citation.contains("<div class=\"csl-right-inline\">"))
-				{
-					citation = citation.substring(citation.indexOf("<div class=\"csl-right-inline\">") + 30);
-					citation = citation.substring(0, citation.indexOf("</div>"));
-				}
-				else if (citation.contains("<div class=\"csl-entry\">"))
-				{
-					citation = citation.substring(citation.indexOf("<div class=\"csl-entry\">") + 23);
-					citation = citation.substring(0, citation.indexOf("</div>"));
-				}
-				else 
-				{
-					citation = citation.trim();
-				}
-				if (logger.isDebugEnabled()) 
-				{
-					logger.debug("Citation: " + citation);
-				}
-				citationList.add(citation);
-			}
-			transformer.setParameter("citations", citationList);
-			transformer.transform(new StreamSource(new StringReader(itemList)), new StreamResult(snippet));
-			if (logger.isDebugEnabled()) 
-			{
-				logger.debug("eSciDoc-Snippet including Ciation: " + snippet);
-			}
-			result = snippet.toString().getBytes("UTF-8");
-
-		} catch (IOException e) {
-			logger.error("Error creating CSL processor", e);
-		} catch (TransformerConfigurationException e) {
-			logger.error("Eror preparing transformation itemList to snippet", e);
-		} catch (TransformerException e) {
-			logger.error("Eror transforming itemList to snippet", e);
-		}
-		return result;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.citation_style_language_manager.CitationStyleLanguageManagerInterface
-	 * #isCitationStyle(java.lang.String)
-	 */
-	@Override
-	public boolean isCitationStyle(String cs) {
-
-		return false;
-	}
-
-	public List<String> getListOfStyles() {
-		List<String> styleList = new ArrayList<String>();
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		try {
-			HttpGet httpget = new HttpGet("http://pubman.mpdl.mpg.de/cone/iso639-3/all?format=jquery");
-
-			System.out.println("Executing request " + httpget.getRequestLine());
-
-			// Create a custom response handler
-			ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
-
-				@Override
-				public String handleResponse(final HttpResponse response)
-						throws ClientProtocolException, IOException {
-					int status = response.getStatusLine().getStatusCode();
-					if (status >= 200 && status < 300) {
-						HttpEntity entity = response.getEntity();
-						return entity != null ? EntityUtils.toString(entity)
-								: null;
-					} else {
-						throw new ClientProtocolException(
-								"Unexpected response status: " + status);
-					}
-				}
-
-			};
-			String responseBody = httpclient.execute(httpget, responseHandler);
-			if (logger.isDebugEnabled()) {
-				logger.debug("-------------------------\n" + responseBody
-						+ "-------------------------\n");
-			}
-
-		} catch (Exception e) {
-			logger.error("Error getting list of citation styles available", e);
-		} finally {
-			try {
-				httpclient.close();
-			} catch (IOException e) {
-				logger.error("error getting ");
-			}
-		}
-		return styleList;
-	}
-
+    public List<String> getListOfStyles()
+    {
+        List<String> styleList = new ArrayList<String>();
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try
+        {
+            HttpGet httpget = new HttpGet("http://pubman.mpdl.mpg.de/cone/iso639-3/all?format=jquery");
+            System.out.println("Executing request " + httpget.getRequestLine());
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>()
+            {
+                @Override
+                public String handleResponse(final HttpResponse response) throws ClientProtocolException, IOException
+                {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300)
+                    {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    }
+                    else
+                    {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+            };
+            String responseBody = httpclient.execute(httpget, responseHandler);
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("-------------------------\n" + responseBody + "-------------------------\n");
+            }
+        }
+        catch (Exception e)
+        {
+            logger.error("Error getting list of citation styles available", e);
+        }
+        finally
+        {
+            try
+            {
+                httpclient.close();
+            }
+            catch (IOException e)
+            {
+                logger.error("error getting ");
+            }
+        }
+        return styleList;
+    }
 }
