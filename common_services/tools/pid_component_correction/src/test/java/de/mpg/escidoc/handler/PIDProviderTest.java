c01dd0d719eb3787ee1228ddb154cb5ac120540e
Gudrun Siedersleben <siedersleben@mpdl.mpg.de>
2015-12-09 10:27:29 +0100

    new test

---
+++ common_services/tools/pid_component_correction/src/test/java/de/mpg/escidoc/handler/PIDProviderTest.java
@@ -0,0 +1,29 @@
+package de.mpg.escidoc.handler;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import javax.naming.NamingException;
+
+import org.junit.Test;
+
+public class PIDProviderTest
+{
+	PIDProvider p = null;
+
+	@Test
+	public void test() throws Exception
+	{
+		p = new PIDProvider();
+		
+		String s = p.updateComponentPid("escidoc:656742", 
+				"6", 
+				"escidoc:656740", 
+				"11858/00-001Z-0000-0023-5323-D", 
+				"7818.html");
+		
+		
+	}
+
+}
