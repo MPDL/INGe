4ff5b0f570bf5e144a8aecd64094d383ee501d9d
Gudrun Siedersleben <siedersleben@mpdl.mpg.de>
2016-07-13 15:35:23 +0200

    simpler test package structure

--- exportmanager/src/test/java/de/mpg/mpdl/inge/exportmanager/test/ExportTest.java
+++ exportmanager/src/test/java/de/mpg/mpdl/inge/exportmanager/test/ExportTest.java
@@ -23,7 +23,7 @@
  * Wissenschaft e.V. All rights reserved. Use is subject to license terms.
  */
 
-package de.mpg.mpdl.inge.exportmanager.test.exportmanager;
+package de.mpg.mpdl.inge.exportmanager.test;
 
 
 import static org.junit.Assert.assertFalse;
