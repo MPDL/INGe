b3ebf30519496ef711657065a64acaadbfd23a72
Gudrun Siedersleben <siedersleben@mpdl.mpg.de>
2016-09-13 16:33:05 +0200

    refactoring

--- cone/src/main/java/de/mpg/mpdl/inge/cone/util/XsltUtils.java
+++ cone/src/main/java/de/mpg/mpdl/inge/cone/util/XsltUtils.java
@@ -1,45 +1,40 @@
 package de.mpg.mpdl.inge.cone.util;
 
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class XsltUtils {
 
+	private XsltUtils() {
 
-  public static boolean validateDate(String date) {
-    SimpleDateFormat yearDf = new SimpleDateFormat("yyyy");
-    yearDf.setLenient(false);
-    SimpleDateFormat yearMonthDf = new SimpleDateFormat("yyyy-MM");
-    yearMonthDf.setLenient(false);
-    SimpleDateFormat yearMonthDayDf = new SimpleDateFormat("yyyy-MM-dd");
-    yearMonthDayDf.setLenient(false);
+	}
 
+	public static boolean validateDate(String date) {
+		SimpleDateFormat yearDf = new SimpleDateFormat("yyyy");
+		yearDf.setLenient(false);
+		SimpleDateFormat yearMonthDf = new SimpleDateFormat("yyyy-MM");
+		yearMonthDf.setLenient(false);
+		SimpleDateFormat yearMonthDayDf = new SimpleDateFormat("yyyy-MM-dd");
+		yearMonthDayDf.setLenient(false);
 
-    try {
-      if (date != null && date.trim().length() == 4) {
-        Date result = yearDf.parse(date);
-        return true;
-      }
+		try {
+			if (date != null && date.trim().length() == 4) {
+				yearDf.parse(date);
+				return true;
+			} else if (date.trim().length() == 7) {
+				yearMonthDf.parse(date);
+				return true;
+			} else if (date.trim().length() == 10) {
+				yearMonthDayDf.parse(date);
+				return true;
+			} else {
+				return false;
+			}
 
-      else if (date.trim().length() == 7) {
-        Date result = yearMonthDf.parse(date);
-        return true;
-      }
+		} catch (ParseException e) {
+			return false;
+		}
 
-
-      else if (date.trim().length() == 10) {
-        Date result = yearMonthDayDf.parse(date);
-        return true;
-      } else {
-        return false;
-      }
-
-    } catch (ParseException e) {
-      return false;
-    }
-
-  }
+	}
 
 }
