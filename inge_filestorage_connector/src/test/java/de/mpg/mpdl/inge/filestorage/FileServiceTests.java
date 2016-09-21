de58fee0d734e95c1a282fb785b2067ad018dd97
przMPDL <przibylla@mpdl.mpg.de>
2016-09-21 12:16:37 +0200

    .

--- inge_filestorage_connector/src/test/java/de/mpg/mpdl/inge/filestorage/FileServiceTests.java
+++ inge_filestorage_connector/src/test/java/de/mpg/mpdl/inge/filestorage/FileServiceTests.java
@@ -135,7 +135,7 @@ public class FileServiceTests {
   }
 
   /**
-   * Test for creating, reading and deleting a file from the glusterfs
+   * Test for creating, reading and deleting a file from glusterfs
    * 
    * @throws IOException
    */
