b79e098d2ed51ff1374e4e3bd690b1ca4cd1c661
walterMPDL <walter@mpdl.mpg.de>
2016-11-28 09:51:57 +0100

    added FileStorageInterfaceConnectorFactory

---
+++ inge_services/src/main/java/de/mpg/mpdl/inge/services/FileStorageInterfaceConnectorFactory.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package de.mpg.mpdl.inge.services;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import de.mpg.mpdl.inge.util.PropertyReader;
+
+/**
+ * Factory for retrieving an implementation of the @FileStorageInterface
+ * 
+ * @author walter
+ * 
+ */
+public class FileStorageInterfaceConnectorFactory {
+  private static final String CONNECTOR_CLASS =
+      "inge.inge_services.file_storage_interface.connector_class";
+
+  public static FileStorageInterface getInstance() throws InstantiationException, IllegalAccessException,
+      ClassNotFoundException, IOException, URISyntaxException {
+    return (FileStorageInterface) Class.forName(CONNECTOR_CLASS).newInstance();
+  }
+}
