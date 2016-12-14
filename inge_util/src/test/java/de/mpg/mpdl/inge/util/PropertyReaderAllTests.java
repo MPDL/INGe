fe89dffc813cd6f34d364e6c0ec1dc3e9b92c096
Gudrun Siedersleben <siedersleben@mpdl.mpg.de>
2016-12-14 16:51:04 +0100

    tests for new PropertyReader

---
+++ inge_util/src/test/java/de/mpg/mpdl/inge/util/PropertyReaderAllTests.java
@@ -0,0 +1,9 @@
+package de.mpg.mpdl.inge.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({PropertyReaderTest.class, PropertyReaderFailureTest.class})
+public class PropertyReaderAllTests {
+}
