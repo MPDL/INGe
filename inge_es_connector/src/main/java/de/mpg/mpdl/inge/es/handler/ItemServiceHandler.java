6fdccafa7431173ec4abe5409e25daf0259f85c8
wilhelmfrank <frank@mpdl.mpg.de>
2017-01-23 15:27:52 +0100

    replace spring beans with pojos

---
+++ inge_es_connector/src/main/java/de/mpg/mpdl/inge/es/handler/ItemServiceHandler.java
@@ -0,0 +1,95 @@
+/**
+ * 
+ */
+package de.mpg.mpdl.inge.es.handler;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.mpg.mpdl.inge.es.connector.ElasticSearchTransportClient;
+import de.mpg.mpdl.inge.es.connector.ElasticSearchTransportClientConnector;
+import de.mpg.mpdl.inge.model.valueobjects.publication.PubItemVO;
+import de.mpg.mpdl.inge.services.IngeServiceException;
+import de.mpg.mpdl.inge.services.ItemInterface;
+import de.mpg.mpdl.inge.util.PropertyReader;
+
+/**
+ * @author frank
+ * 
+ */
+public class ItemServiceHandler implements ItemInterface {
+
+  private ObjectMapper mapper = new ObjectMapper();
+  private final String indexName = PropertyReader.getProperty("item_index_name");
+  private final String indexType = PropertyReader.getProperty("item_index_type");
+
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * de.mpg.mpdl.inge.services.ItemInterface#createItem(de.mpg.mpdl.inge.model.valueobjects.ItemVO,
+   * java.lang.String)
+   */
+  @Override
+  public String createItem(PubItemVO item, String itemId) throws IngeServiceException {
+    byte[] voAsBytes;
+    try {
+      voAsBytes = mapper.writeValueAsBytes(item);
+      return ElasticSearchTransportClient.INSTANCE.index(indexName, indexType, itemId, voAsBytes);
+    } catch (JsonProcessingException e) {
+      throw new IngeServiceException(e.getMessage(), e.getCause());
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.mpg.mpdl.inge.services.ItemInterface#readItem(java.lang.String)
+   */
+  @Override
+  public PubItemVO readItem(String itemId) throws IngeServiceException {
+    byte[] voAsBytes = ElasticSearchTransportClient.INSTANCE.get(indexName, indexType, itemId);
+    try {
+      PubItemVO item = mapper.readValue(voAsBytes, PubItemVO.class);
+      return item;
+    } catch (IOException e) {
+      throw new IngeServiceException(e.getMessage(), e.getCause());
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * de.mpg.mpdl.inge.services.ItemInterface#updateItem(de.mpg.mpdl.inge.model.valueobjects.ItemVO,
+   * java.lang.String, boolean)
+   */
+  @Override
+  public String updateItem(PubItemVO item, String itemId) throws IngeServiceException {
+    byte[] voAsBytes;
+    try {
+      voAsBytes = mapper.writeValueAsBytes(item);
+      return ElasticSearchTransportClient.INSTANCE.update(indexName, indexType, itemId, voAsBytes);
+    } catch (JsonProcessingException e) {
+      throw new IngeServiceException(e.getMessage(), e.getCause());
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.mpg.mpdl.inge.services.ItemInterface#deleteItem(java.lang.String)
+   */
+  @Override
+  public String deleteItem(String itemId) {
+    return ElasticSearchTransportClient.INSTANCE.delete(indexName, indexType, itemId);
+  }
+
+}
---
+++ inge_es_connector/src/main/java/de/mpg/mpdl/inge/es/handler/OrganizationServiceHandler.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package de.mpg.mpdl.inge.es.handler;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.mpg.mpdl.inge.es.connector.ElasticSearchTransportClient;
+import de.mpg.mpdl.inge.es.connector.ElasticSearchTransportClientConnector;
+import de.mpg.mpdl.inge.model.valueobjects.AffiliationVO;
+import de.mpg.mpdl.inge.services.IngeServiceException;
+import de.mpg.mpdl.inge.services.OrganizationInterface;
+import de.mpg.mpdl.inge.util.PropertyReader;
+
+/**
+ * @author frank
+ * 
+ */
+public class OrganizationServiceHandler implements OrganizationInterface {
+
+  private ObjectMapper mapper = new ObjectMapper();
+  private final String indexName = PropertyReader.getProperty("organization_index_name");
+  private final String indexType = PropertyReader.getProperty("organization_index_type");
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.mpg.mpdl.inge.services.OrganizationInterface#createOrganization(de.mpg.mpdl.inge.model.
+   * valueobjects.AffiliationVO)
+   */
+  @Override
+  public String createOrganization(AffiliationVO organization, String organizationId)
+      throws IngeServiceException {
+    byte[] voAsBytes;
+    try {
+      voAsBytes = mapper.writeValueAsBytes(organization);
+      return ElasticSearchTransportClient.INSTANCE.index(indexName, indexType, organizationId,
+          voAsBytes);
+    } catch (JsonProcessingException e) {
+      throw new IngeServiceException(e.getMessage(), e.getCause());
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.mpg.mpdl.inge.services.OrganizationInterface#readOrganization(java.lang.String)
+   */
+  @Override
+  public AffiliationVO readOrganization(String organizationId) throws IngeServiceException {
+    byte[] voAsBytes =
+        ElasticSearchTransportClient.INSTANCE.get(indexName, indexType, organizationId);
+    try {
+      AffiliationVO organization = mapper.readValue(voAsBytes, AffiliationVO.class);
+      return organization;
+    } catch (IOException e) {
+      throw new IngeServiceException(e.getMessage(), e.getCause());
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.mpg.mpdl.inge.services.OrganizationInterface#updateOrganization(de.mpg.mpdl.inge.model.
+   * valueobjects.AffiliationVO)
+   */
+  @Override
+  public String updateOrganization(AffiliationVO organization, String organizationId)
+      throws IngeServiceException {
+    byte[] voAsBytes;
+    try {
+      voAsBytes = mapper.writeValueAsBytes(organization);
+      return ElasticSearchTransportClient.INSTANCE.update(indexName, indexType, organizationId,
+          voAsBytes);
+    } catch (JsonProcessingException e) {
+      throw new IngeServiceException(e.getMessage(), e.getCause());
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see de.mpg.mpdl.inge.services.OrganizationInterface#deleteOrganization(java.lang.String)
+   */
+  @Override
+  public String deleteOrganization(String organizationId) {
+    return ElasticSearchTransportClient.INSTANCE.delete(indexName, indexType, organizationId);
+  }
+}
