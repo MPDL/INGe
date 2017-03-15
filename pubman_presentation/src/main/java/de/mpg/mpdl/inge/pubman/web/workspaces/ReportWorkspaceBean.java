52064638920f147affdf935e90a922eb5fe805d7
Gudrun Siedersleben <siedersleben@mpdl.mpg.de>
2017-03-15 13:28:17 +0100

    ...

--- pubman_presentation/src/main/java/de/mpg/mpdl/inge/pubman/web/workspaces/ReportWorkspaceBean.java
+++ pubman_presentation/src/main/java/de/mpg/mpdl/inge/pubman/web/workspaces/ReportWorkspaceBean.java
@@ -21,7 +21,7 @@ import javax.servlet.ServletOutputStream;
 import org.apache.log4j.Logger;
 
 import de.escidoc.www.services.oum.OrganizationalUnitHandler;
-import de.mpg.mpdl.inge.citationmanager.CitationStyleManager;
+import de.mpg.mpdl.inge.citationmanager.CitationStyleExecuterService;
 import de.mpg.mpdl.inge.dataacquisition.Util;
 import de.mpg.mpdl.inge.framework.ServiceLocator;
 import de.mpg.mpdl.inge.model.valueobjects.AffiliationVO;
@@ -270,7 +270,7 @@ public class ReportWorkspaceBean extends FacesBean {
     byte[] exportData = null;
     try {
       exportData =
-          CitationStyleManager.getOutput(itemListAsString, new ExportFormatVO(FormatType.LAYOUT,
+          CitationStyleExecuterService.getOutput(itemListAsString, new ExportFormatVO(FormatType.LAYOUT,
               csExportFormat, csOutputFormat));
     } catch (Exception e) {
       logger.error("Error when trying to find citation service.", e);
