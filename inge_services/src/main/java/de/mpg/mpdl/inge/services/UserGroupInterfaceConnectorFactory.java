92a0793b088591bc7f2d9d0e8fa2908b0d4a400a
walterMPDL <walter@mpdl.mpg.de>
2017-03-16 09:50:44 +0100

    Bugfix interfaces

--- inge_services/src/main/java/de/mpg/mpdl/inge/services/UserGroupInterfaceConnectorFactory.java
+++ inge_services/src/main/java/de/mpg/mpdl/inge/services/UserGroupInterfaceConnectorFactory.java
@@ -6,6 +6,8 @@ package de.mpg.mpdl.inge.services;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import de.mpg.mpdl.inge.util.PropertyReader;
+
 /**
  * Factory for retrieving an implementation of the @UserGroupInterface
  * 
@@ -13,11 +15,12 @@ import java.net.URISyntaxException;
  * 
  */
 public class UserGroupInterfaceConnectorFactory {
-  private static final String CONNECTOR_CLASS =
+  private static final String CONNECTOR_CLASS_PROPERTY =
       "inge.inge_services.usergroup_interface.connector_class";
 
   public static UserGroupInterface getInstance() throws InstantiationException,
       IllegalAccessException, ClassNotFoundException, IOException, URISyntaxException {
-    return (UserGroupInterface) Class.forName(CONNECTOR_CLASS).newInstance();
+    return (UserGroupInterface) Class.forName(PropertyReader.getProperty(CONNECTOR_CLASS_PROPERTY))
+        .newInstance();
   }
 }
--- inge_services/src/main/java/de/mpg/mpdl/inge/services/UserInterfaceConnectorFactory.java
+++ inge_services/src/main/java/de/mpg/mpdl/inge/services/UserInterfaceConnectorFactory.java
@@ -6,6 +6,8 @@ package de.mpg.mpdl.inge.services;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import de.mpg.mpdl.inge.util.PropertyReader;
+
 /**
  * Factory for retrieving an implementation of the @UserInterface
  * 
@@ -13,10 +15,11 @@ import java.net.URISyntaxException;
  * 
  */
 public class UserInterfaceConnectorFactory {
-  private static final String CONNECTOR_CLASS = "inge.inge_services.user_interface.connector_class";
+  private static final String CONNECTOR_CLASS_PROPERTY = "inge.inge_services.user_interface.connector_class";
 
   public static UserInterface getInstance() throws InstantiationException, IllegalAccessException,
       ClassNotFoundException, IOException, URISyntaxException {
-    return (UserInterface) Class.forName(CONNECTOR_CLASS).newInstance();
+    return (UserInterface) Class.forName(PropertyReader.getProperty(CONNECTOR_CLASS_PROPERTY))
+        .newInstance();
   }
 }
