0c3c3a789e2a8eb73976a332f8a8f12ce8550803
przMPDL <przibylla@mpdl.mpg.de>
2017-04-06 15:19:29 +0200

    some changes

--- citationmanager/src/test/java/de/mpg/mpdl/inge/citationmanager/CitationStyleManagerTest.java
+++ citationmanager/src/test/java/de/mpg/mpdl/inge/citationmanager/CitationStyleManagerTest.java
@@ -3,38 +3,15 @@
  */
 package de.mpg.mpdl.inge.citationmanager;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
-import java.io.StringWriter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.log4j.Logger;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
 import org.xml.sax.SAXException;
 
-import de.mpg.mpdl.inge.citationmanager.utils.Utils;
-import de.mpg.mpdl.inge.citationmanager.utils.XmlHelper;
 import de.mpg.mpdl.inge.citationmanager.xslt.CitationStyleManagerImpl;
-import de.mpg.mpdl.inge.model.valueobjects.ExportFormatVO;
-import de.mpg.mpdl.inge.model.valueobjects.ExportFormatVO.FormatType;
-import de.mpg.mpdl.inge.transformation.Transformer;
-import de.mpg.mpdl.inge.transformation.TransformerFactory;
-import de.mpg.mpdl.inge.transformation.results.TransformerStreamResult;
-import de.mpg.mpdl.inge.transformation.sources.TransformerStreamSource;
-import de.mpg.mpdl.inge.transformation.util.Format;
-import de.mpg.mpdl.inge.util.ResourceUtil;
 
 /**
  * @author endres
