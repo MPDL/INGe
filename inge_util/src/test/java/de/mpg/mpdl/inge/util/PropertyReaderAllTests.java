cd4cd41a5b0b8ad0c9c12973892a8e91960f92d1
Gudrun Siedersleben <siedersleben@mpdl.mpg.de>
2017-05-03 10:59:16 +0200

    removed PropertyReaderAllTests

--- inge_util/src/test/java/de/mpg/mpdl/inge/util/PropertyReaderAllTests.java
+++
@@ -1,9 +0,0 @@
-package de.mpg.mpdl.inge.util;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({PropertyReaderTest.class, PropertyReaderFailureTest.class})
-public class PropertyReaderAllTests {
-}
