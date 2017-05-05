bd11bd8c3910228ab5a0c5c257c70fd1bcd073a6
przMPDL <przibylla@mpdl.mpg.de>
2017-05-05 11:33:11 +0200

    some formatting

--- inge_validation/src/main/java/de/mpg/mpdl/inge/inge_validation/validator/SourceCreatorsRoleRequiredValidator.java
+++ inge_validation/src/main/java/de/mpg/mpdl/inge/inge_validation/validator/SourceCreatorsRoleRequiredValidator.java
@@ -92,7 +92,6 @@ public class SourceCreatorsRoleRequiredValidator extends ValidatorHandler<List<S
                         }
                       }
 
-
                       z++;
                     } // for
 
