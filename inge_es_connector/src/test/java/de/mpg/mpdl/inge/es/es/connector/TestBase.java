b60cbd3b48444da635412d8fd508747810d0545b
przMPDL <przibylla@mpdl.mpg.de>
2017-05-12 10:49:35 +0200

    Tests für INGE_ES_CONNECTOR Korrektur

--- inge_es_connector/src/test/java/de/mpg/mpdl/inge/es/es/connector/TestBase.java
+++ inge_es_connector/src/test/java/de/mpg/mpdl/inge/es/es/connector/TestBase.java
@@ -3,14 +3,11 @@ package de.mpg.mpdl.inge.es.es.connector;
 import java.io.IOException;
 import java.util.Date;
 
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.mpg.mpdl.inge.es.connector.ModelMapper;
 import de.mpg.mpdl.inge.es.spring.AppConfig;
@@ -60,7 +57,6 @@ import de.mpg.mpdl.inge.model.valueobjects.publication.PubItemVO;
 import de.mpg.mpdl.inge.model.valueobjects.publication.PublicationAdminDescriptorVO;
 import de.mpg.mpdl.inge.model.valueobjects.publication.PublicationAdminDescriptorVO.Workflow;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 public class TestBase {
   private static final Date DATE = new Date();
@@ -547,5 +543,4 @@ public class TestBase {
     PubItemVO item = mapper.readValue(src, PubItemVO.class);
     return item;
   }
-
 }
