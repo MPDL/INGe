5e86629f640ca4807f0f62ecd8be5c1695643184
walterMPDL <walter@mpdl.mpg.de>
2017-06-19 09:53:08 +0200

    cleanup

--- pubman_presentation/src/main/java/de/mpg/mpdl/inge/pubman/web/spring/WebAppInitializer.java
+++ pubman_presentation/src/main/java/de/mpg/mpdl/inge/pubman/web/spring/WebAppInitializer.java
@@ -15,29 +15,14 @@ import de.mpg.mpdl.inge.service.spring.AppConfigPubmanLogic;
 
 public class WebAppInitializer implements WebApplicationInitializer {
   public void onStartup(ServletContext servletContext) throws ServletException {
-
-
-    /*
-     * AnnotationConfigWebApplicationContext rootCtx = new AnnotationConfigWebApplicationContext();
-     * rootCtx.register(AppConfigPubmanLogic.class); rootCtx.setBeanName("parent.context");
-     * servletContext.addListener(new ContextLoaderListener(rootCtx));
-     */
-
     AnnotationConfigWebApplicationContext myctx = new AnnotationConfigWebApplicationContext();
     myctx.register(AppConfigPubmanPresentation.class);
-    // myctx.setParent(rootCtx);
-
     myctx.setServletContext(servletContext);
     servletContext.addListener(new ContextLoaderListener(myctx));
-
     servletContext.addListener(new RequestContextListener());
 
     // Use ear.context (pubman_logic) as shared context between all webapps in the ear.
     // ear.context is defined in beanRefContext.xml in module pubman_logic
     servletContext.setInitParameter(ContextLoader.LOCATOR_FACTORY_KEY_PARAM, "ear.context");
-    /*
-     * Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
-     * dynamic.addMapping("/"); dynamic.setLoadOnStartup(1);
-     */
   }
 }
