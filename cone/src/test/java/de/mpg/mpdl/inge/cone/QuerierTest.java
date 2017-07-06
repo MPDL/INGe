e7ae20ac4e577f2263f07556498dac1ccf841973
Gudrun Siedersleben <siedersleben@mpdl.mpg.de>
2017-07-06 16:42:54 +0200

    asserts enhanced

--- cone/src/test/java/de/mpg/mpdl/inge/cone/QuerierTest.java
+++ cone/src/test/java/de/mpg/mpdl/inge/cone/QuerierTest.java
@@ -97,7 +97,9 @@ public class QuerierTest {
               || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("abstr")
               || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("journal")
               || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("j.")
-              || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("adv"));
+              || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("adv")
+              || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("society")
+              || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("acta"));
     }
   }
 
@@ -113,7 +115,13 @@ public class QuerierTest {
 
     for (Describable pair : results) {
       assertTrue("Result does not contain query string 'of': " + ((Pair<?>) pair).getValue(),
-          ((Pair<?>) pair).getValue().toString().toLowerCase().contains("of"));
+          ((Pair<?>) pair).getValue().toString().toLowerCase().contains("of")
+          || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("abstr")
+          || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("journal")
+          || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("j.")
+          || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("adv")
+          || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("society")
+          || ((Pair<?>) pair).getValue().toString().toLowerCase().contains("acta"));
     }
   }
 
