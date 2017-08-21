568671db100952d38a1fbce07d1f42c18a03a424
przMPDL <przibylla@mpdl.mpg.de>
2017-08-21 10:54:55 +0200

    ImportSurveyorTask as Spring Scheduler

--- pubman_presentation/src/main/java/de/mpg/mpdl/inge/pubman/web/spring/AppConfigPubmanPresentation.java
+++ pubman_presentation/src/main/java/de/mpg/mpdl/inge/pubman/web/spring/AppConfigPubmanPresentation.java
@@ -2,12 +2,10 @@ package de.mpg.mpdl.inge.pubman.web.spring;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 @ComponentScan("de.mpg.mpdl.inge.pubman.web")
 public class AppConfigPubmanPresentation {
-
-
-
 }
