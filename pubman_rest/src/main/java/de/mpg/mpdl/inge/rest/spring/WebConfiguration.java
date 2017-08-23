097c8c9b66647c84fbe40742748c9b8edc53f998
przMPDL <przibylla@mpdl.mpg.de>
2017-08-23 11:33:26 +0200

    MessageConverters an der falschen Stelle?

--- pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/spring/WebConfiguration.java
+++ pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/spring/WebConfiguration.java
@@ -1,12 +1,8 @@
 package de.mpg.mpdl.inge.rest.spring;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -15,11 +11,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 @EnableWebMvc
@@ -38,20 +29,20 @@ public class WebConfiguration extends RepositoryRestMvcConfiguration {
     // super.addCorsMappings(registry);
   }
 
-  @Override
-  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-    ObjectMapper objectMapper = new ObjectMapper();
-
-    objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
-    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    objectMapper.registerModule(new JavaTimeModule());
-
-    MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-    converter.setObjectMapper(objectMapper);
-    converters.add(converter);
-    super.configureMessageConverters(converters);
-  }
+  // @Override
+  // public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+  // ObjectMapper objectMapper = new ObjectMapper();
+  //
+  // objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+  // objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+  // objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  // objectMapper.registerModule(new JavaTimeModule());
+  //
+  // MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+  // converter.setObjectMapper(objectMapper);
+  // converters.add(converter);
+  // super.configureMessageConverters(converters);
+  // }
 
   @Override
   public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
