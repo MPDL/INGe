6578a32f8738293aaac4750136c1f80ea4427285
przMPDL <przibylla@mpdl.mpg.de>
2017-08-23 11:36:17 +0200

    UNAPI stuff

--- pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/web/controller/UnapiRestController.java
+++ pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/web/controller/UnapiRestController.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 import org.apache.xmlbeans.XmlOptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +25,7 @@ import de.mpg.mpdl.inge.service.exceptions.AuthorizationException;
 import de.mpg.mpdl.inge.service.exceptions.IngeApplicationException;
 import de.mpg.mpdl.inge.service.pubman.ItemTransformingService;
 import de.mpg.mpdl.inge.service.pubman.PubItemService;
+import de.mpg.mpdl.inge.transformation.TransformerFactory;
 import de.mpg.mpdl.inge.transformation.TransformerFactory.FORMAT;
 import de.mpg.mpdl.inge.transformation.exceptions.TransformationException;
 import noNamespace.FormatType;
@@ -42,7 +45,7 @@ public class UnapiRestController {
     this.its = its;
   }
 
-  @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/xml")
+  @RequestMapping(value = "", method = RequestMethod.GET)
   public ResponseEntity<String> unapi( //
       @RequestParam(value = "id", required = false) String identifier, //
       @RequestParam(value = "show", required = false) Boolean show, //
@@ -59,6 +62,7 @@ public class UnapiRestController {
       FormatsDocument xmlFormatsDoc = FormatsDocument.Factory.newInstance();
       FormatsType xmlFormats = xmlFormatsDoc.addNewFormats();
       FormatType xmlFormat = xmlFormats.addNewFormat();
+      // TODO: hier direkt mit TransformerFactory.FORMAT.ESCIDOC_ITEM_V3_XML arbeiten
       String esciDocFormatName = Util.getInternalFormat();
       xmlFormat.setName(esciDocFormatName);
       xmlFormat.setType(Util.getDefaultMimeType(esciDocFormatName));
@@ -74,7 +78,10 @@ public class UnapiRestController {
 
       srResponse = new String(baos.toByteArray());
 
-      return new ResponseEntity<String>(srResponse, HttpStatus.OK);
+      HttpHeaders headers = new HttpHeaders();
+      headers.setContentType(MediaType.APPLICATION_XML);
+
+      return new ResponseEntity<String>(srResponse, headers, HttpStatus.OK);
 
       // public byte[] unapi(String identifier, boolean show)
     } else if (identifier != null && show != null && format == null) {
@@ -97,6 +104,20 @@ public class UnapiRestController {
       metadataV.add(sourceFormat);
       metadataV.addAll(Util.getTransformFormats(metadataV));
 
+      // TODO
+      // this.its.getAllTargetFormatsFor(TransformerFactory.FORMAT.ESCIDOC_ITEM_V3_XML); -> ersetzt
+      // Util.getTransformFormats()
+      // Benennung der Formate? -> PubmanQA liefert:
+      // <formats id="escidoc:1:1">
+      // <format name="eSciDoc-publication-item" type="application/xml"/>
+      // <format name="BibTex" type="text/plain"/>
+      // <format name="EndNote" type="text/plain"/>
+      // <format name="oai_dc" type="application/xml"/>
+      // <format name="html-meta-tags-highwire-press-citation" type="text/html"/>
+      // <format name="html-meta-tags-dc" type="text/html"/>
+      // <format name="Coins" type="text/plain"/>
+      // </formats>
+
       for (int i = 0; i < metadataV.size(); i++) {
         MetadataVO md = metadataV.get(i);
         FormatType xmlFormat = xmlFormats.addNewFormat();
@@ -118,17 +139,26 @@ public class UnapiRestController {
 
       srResponse = new String(baos.toByteArray());
 
-      return new ResponseEntity<String>(srResponse, HttpStatus.OK);
+      HttpHeaders headers = new HttpHeaders();
+      headers.setContentType(MediaType.APPLICATION_XML);
+
+      return new ResponseEntity<String>(srResponse, headers, HttpStatus.OK);
 
       // public byte[] unapi(String identifier, String format)
     } else if (identifier != null && show == null && format != null) {
       PubItemVO pubItemVO = this.pis.get(identifier, null);
 
+      TransformerFactory.FORMAT myFormat = TransformerFactory.FORMAT.valueOf(format);
+
       try {
-        srResponse = this.its.transformPubItemTo(FORMAT.valueOf(format), pubItemVO);
+        srResponse = this.its.transformPubItemTo(myFormat, pubItemVO);
       } catch (TransformationException e) {
         throw new IngeApplicationException(e);
       }
+
+      HttpHeaders headers = new HttpHeaders();
+      // TODO: In TransformerFactory die Medientypen deklarieren
+      headers.setContentType(MediaType.parseMediaType(myFormat.getType()));
 
       return new ResponseEntity<String>(srResponse, HttpStatus.OK);
     }
