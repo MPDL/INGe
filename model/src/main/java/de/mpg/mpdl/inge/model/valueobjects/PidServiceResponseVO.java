ee141cbc728e25d93a45fbe7b67c5402c51a0b70
przMPDL <przibylla@mpdl.mpg.de>
2017-11-07 08:15:44 +0100

    Anpassung PidServiceResponseVO

--- model/src/main/java/de/mpg/mpdl/inge/model/valueobjects/PidServiceResponseVO.java
+++ model/src/main/java/de/mpg/mpdl/inge/model/valueobjects/PidServiceResponseVO.java
@@ -2,25 +2,10 @@ package de.mpg.mpdl.inge.model.valueobjects;
 
 @SuppressWarnings("serial")
 public class PidServiceResponseVO extends ValueObject {
-  private String action;
   protected String identifier;
   protected String url;
-  private String creator;
-  private String userUid;
-  private String message;
-  private String institute;
-  private String contact;
 
   public PidServiceResponseVO() {
-    super();
-  }
-
-  public String getAction() {
-    return action;
-  }
-
-  public void setAction(String action) {
-    this.action = action;
   }
 
   public String getUrl() {
@@ -39,50 +24,8 @@ public class PidServiceResponseVO extends ValueObject {
     this.identifier = identifier;
   }
 
-  public String getCreator() {
-    return creator;
-  }
-
-  public void setCreator(String creator) {
-    this.creator = creator;
-  }
-
-  public String getUserUid() {
-    return userUid;
-  }
-
-  public void setUserUid(String userUid) {
-    this.userUid = userUid;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
-
-  public String getInstitute() {
-    return institute;
-  }
-
-  public void setInstitute(String institute) {
-    this.institute = institute;
-  }
-
-  public String getContact() {
-    return contact;
-  }
-
-  public void setContact(String contact) {
-    this.contact = contact;
-  }
-
   @Override
   public String toString() {
-    return "PidServiceResponseVO [action=" + action + ", identifier=" + identifier + ", url=" + url
-        + ", creator=" + creator + ", userUid=" + userUid + ", message=" + message + ", institute="
-        + institute + ", contact=" + contact + "]";
+    return "PidServiceResponseVO [identifier=" + identifier + ", url=" + url + "]";
   }
 }
