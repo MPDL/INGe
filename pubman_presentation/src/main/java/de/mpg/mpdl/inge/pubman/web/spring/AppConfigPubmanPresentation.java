7bf5d0ec92ef321cc8a8525e7ef2f5f388c096a3
przMPDL <przibylla@mpdl.mpg.de>
2017-11-16 11:53:43 +0100

    Validation as Spring service

--- pubman_presentation/src/main/java/de/mpg/mpdl/inge/pubman/web/spring/AppConfigPubmanPresentation.java
+++ pubman_presentation/src/main/java/de/mpg/mpdl/inge/pubman/web/spring/AppConfigPubmanPresentation.java
@@ -2,10 +2,14 @@ package de.mpg.mpdl.inge.pubman.web.spring;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import de.mpg.mpdl.inge.inge_validation.spring.AppConfigIngeValidation;
 
 @Configuration
 @EnableScheduling
 @ComponentScan("de.mpg.mpdl.inge.pubman.web")
+@Import({AppConfigIngeValidation.class})
 public class AppConfigPubmanPresentation {
 }
