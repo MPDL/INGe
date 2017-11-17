81e64323d85ebb5f0f4d8d4aa0164f375626c0c4
przMPDL <przibylla@mpdl.mpg.de>
2017-11-17 11:26:24 +0100

    CONE Cache: manuelles Refreshen ermöglichen

--- cone_cache_presentation/src/main/java/de/mpg/mpdl/inge/cone_cache/web/InitializerServlet.java
+++ cone_cache_presentation/src/main/java/de/mpg/mpdl/inge/cone_cache/web/InitializerServlet.java
@@ -1,7 +1,11 @@
 package de.mpg.mpdl.inge.cone_cache.web;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import de.mpg.mpdl.inge.cone_cache.ConeCacheInitTask;
 
@@ -16,6 +20,15 @@ public class InitializerServlet extends HttpServlet {
 
     this.initTask = new ConeCacheInitTask();
     this.initTask.start();
+  }
+
+  // TODO: Authorisierung
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+      IOException {
+    this.initTask = new ConeCacheInitTask();
+    this.initTask.start();
+    resp.getWriter().write("CONE CACHE refresh requested!");
   }
 
   @Override
