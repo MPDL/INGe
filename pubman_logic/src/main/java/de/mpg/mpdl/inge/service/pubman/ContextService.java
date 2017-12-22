a0027fc61b3a35271b0338d0f4668a9d3f2399e0
haarli <haarlaender@mpdl.mpg.de>
2017-12-22 16:14:06 +0100

    Changed Interfaces to new model

--- pubman_logic/src/main/java/de/mpg/mpdl/inge/service/pubman/ContextService.java
+++ pubman_logic/src/main/java/de/mpg/mpdl/inge/service/pubman/ContextService.java
@@ -2,18 +2,19 @@ package de.mpg.mpdl.inge.service.pubman;
 
 import java.util.Date;
 
+import de.mpg.mpdl.inge.model.db.valueobjects.ContextDbVO;
 import de.mpg.mpdl.inge.model.exception.IngeTechnicalException;
 import de.mpg.mpdl.inge.model.valueobjects.ContextVO;
 import de.mpg.mpdl.inge.service.exceptions.AuthenticationException;
 import de.mpg.mpdl.inge.service.exceptions.AuthorizationException;
 import de.mpg.mpdl.inge.service.exceptions.IngeApplicationException;
 
-public interface ContextService extends GenericService<ContextVO, String> {
+public interface ContextService extends GenericService<ContextDbVO, String> {
 
-  public ContextVO open(String contextId, Date modificationDate, String authenticationToken)
+  public ContextDbVO open(String contextId, Date modificationDate, String authenticationToken)
       throws IngeTechnicalException, AuthenticationException, AuthorizationException, IngeApplicationException;
 
-  public ContextVO close(String contextId, Date modificationDate, String authenticationToken)
+  public ContextDbVO close(String contextId, Date modificationDate, String authenticationToken)
       throws IngeTechnicalException, AuthenticationException, AuthorizationException, IngeApplicationException;
 
 }
--- pubman_logic/src/main/java/de/mpg/mpdl/inge/service/pubman/PubItemService.java
+++ pubman_logic/src/main/java/de/mpg/mpdl/inge/service/pubman/PubItemService.java
@@ -3,6 +3,7 @@ package de.mpg.mpdl.inge.service.pubman;
 import java.util.Date;
 import java.util.List;
 
+import de.mpg.mpdl.inge.model.db.valueobjects.PubItemVersionDbVO;
 import de.mpg.mpdl.inge.model.exception.IngeTechnicalException;
 import de.mpg.mpdl.inge.model.valueobjects.VersionHistoryEntryVO;
 import de.mpg.mpdl.inge.model.valueobjects.publication.PubItemVO;
@@ -10,17 +11,17 @@ import de.mpg.mpdl.inge.service.exceptions.AuthenticationException;
 import de.mpg.mpdl.inge.service.exceptions.AuthorizationException;
 import de.mpg.mpdl.inge.service.exceptions.IngeApplicationException;
 
-public interface PubItemService extends GenericService<PubItemVO, String> {
-  public PubItemVO submitPubItem(String pubItemId, Date modificationDate, String message, String authenticationToken)
+public interface PubItemService extends GenericService<PubItemVersionDbVO, String> {
+  public PubItemVersionDbVO submitPubItem(String pubItemId, Date modificationDate, String message, String authenticationToken)
       throws IngeTechnicalException, AuthenticationException, AuthorizationException, IngeApplicationException;
 
-  public PubItemVO releasePubItem(String pubItemId, Date modificationDate, String message, String authenticationToken)
+  public PubItemVersionDbVO releasePubItem(String pubItemId, Date modificationDate, String message, String authenticationToken)
       throws IngeTechnicalException, AuthenticationException, AuthorizationException, IngeApplicationException;
 
-  public PubItemVO withdrawPubItem(String pubItemId, Date modificationDate, String message, String authenticationToken)
+  public PubItemVersionDbVO withdrawPubItem(String pubItemId, Date modificationDate, String message, String authenticationToken)
       throws IngeTechnicalException, AuthenticationException, AuthorizationException, IngeApplicationException;
 
-  public PubItemVO revisePubItem(String pubItemId, Date modificationDate, String message, String authenticationToken)
+  public PubItemVersionDbVO revisePubItem(String pubItemId, Date modificationDate, String message, String authenticationToken)
       throws IngeTechnicalException, AuthenticationException, AuthorizationException, IngeApplicationException;
 
   public List<VersionHistoryEntryVO> getVersionHistory(String pubItemId, String authenticationToken)
