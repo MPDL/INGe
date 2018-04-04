ffd62eceff842186a03c832395dc9ccf3ac0ee36
sieders <siedersleben@mpdl.mpg.de>
2018-04-04 14:20:04 +0200

    optimize imports

--- pubman_logic/src/test/java/de/mpg/mpdl/inge/service/spring/AppConfigPubmanLogicTest.java
+++ pubman_logic/src/test/java/de/mpg/mpdl/inge/service/spring/AppConfigPubmanLogicTest.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
