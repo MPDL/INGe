b3f51869d3a4a2aa4172f0ba93896fd1b71cb7a8
haarli <haarlaender@mpdl.mpg.de>
2018-07-26 11:02:34 +0200

    Moved admintool to separate project

--- admintool/src/main/angular/src/app/base/directives/documentclick.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentclickDirective } from './documentclick.directive';
-
-describe('DocumentclickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DocumentclickDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- admintool/src/main/angular/src/app/base/directives/value-not-allowed.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValueNotAllowedDirective } from './value-not-allowed.directive';
-
-describe('ValueNotAllowedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValueNotAllowedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
