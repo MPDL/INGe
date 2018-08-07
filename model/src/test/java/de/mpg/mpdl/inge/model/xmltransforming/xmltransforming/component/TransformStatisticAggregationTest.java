7c1cc6f194c3b69ac4b423eda9fe1eaffddcb2c5
przMPDL <wurzelpuzz@users.noreply.github.com>
2018-08-07 15:54:51 +0200

    UTF-8 stuff

--- model/src/test/java/de/mpg/mpdl/inge/model/xmltransforming/xmltransforming/component/TransformStatisticAggregationTest.java
+++ model/src/test/java/de/mpg/mpdl/inge/model/xmltransforming/xmltransforming/component/TransformStatisticAggregationTest.java
@@ -20,7 +20,7 @@
 
 /*
  * Copyright 2006-2012 Fachinformationszentrum Karlsruhe Gesellschaft für
- * wissenschaftlich-technische Information mbH and Max-Planck- Gesellschaft zur F�rderung der
+ * wissenschaftlich-technische Information mbH and Max-Planck- Gesellschaft zur Förderung der
  * Wissenschaft e.V. All rights reserved. Use is subject to license terms.
  */
 package de.mpg.mpdl.inge.model.xmltransforming.xmltransforming.component;
