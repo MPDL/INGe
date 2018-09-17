d715fc74c810fd1b10d209bde2cb972eb9424061
przMPDL <wurzelpuzz@users.noreply.github.com>
2018-09-17 09:39:28 +0200

    And another try

--- transformationManager/src/main/java/de/mpg/mpdl/inge/transformation/transformers/CitationTransformer.java
+++ transformationManager/src/main/java/de/mpg/mpdl/inge/transformation/transformers/CitationTransformer.java
@@ -143,24 +143,24 @@ public class CitationTransformer extends SingleTransformer implements ChainableT
 
       // TODO: Viel schöner machen!
       if (TransformerFactory.FORMAT.PDF.equals(getTargetFormat())) {
-        //        for (Entry<String, PhysicalFont> entry : PhysicalFonts.getPhysicalFonts().entrySet()) {
-        //          System.out.println(entry);
-        //        }
+                for (Entry<String, PhysicalFont> entry : PhysicalFonts.getPhysicalFonts().entrySet()) {
+                  System.out.println(entry);
+                }
         PhysicalFont font = PhysicalFonts.getPhysicalFonts().get("lato");
         if (font != null) {
           Mapper fontMapper = new IdentityPlusMapper();
-          //          for (Entry<String, PhysicalFont> entry : fontMapper.getFontMappings().entrySet()) {
-          //            System.out.println(entry);
-          //          }
+                    for (Entry<String, PhysicalFont> entry : fontMapper.getFontMappings().entrySet()) {
+                      System.out.println(entry);
+                    }
           fontMapper.getFontMappings().put("Calibri", font);
           fontMapper.getFontMappings().put("MS Gothic", font);
           fontMapper.getFontMappings().put("Times New Roman", font);
           fontMapper.getFontMappings().put("calibri", font);
           fontMapper.getFontMappings().put("ms gothic", font);
           fontMapper.getFontMappings().put("times new roman", font);
-          //          for (Entry<String, PhysicalFont> entry : fontMapper.getFontMappings().entrySet()) {
-          //            System.out.println(entry);
-          //          }
+                    for (Entry<String, PhysicalFont> entry : fontMapper.getFontMappings().entrySet()) {
+                      System.out.println(entry);
+                    }
           wordOutputDoc.setFontMapper(fontMapper);
         }
       }
