ea04b49ae0a6f3ed59d677f982b659cae4f9642d
przMPDL <wurzelpuzz@users.noreply.github.com>
2018-09-25 11:10:01 +0200

    Fix pdf

--- transformationManager/src/main/java/de/mpg/mpdl/inge/transformation/transformers/CitationTransformer.java
+++ transformationManager/src/main/java/de/mpg/mpdl/inge/transformation/transformers/CitationTransformer.java
@@ -147,7 +147,7 @@ public class CitationTransformer extends SingleTransformer implements ChainableT
           System.out.println(entry);
         }
 
-        PhysicalFont font = PhysicalFonts.getPhysicalFonts().get("liberation");
+        PhysicalFont font = PhysicalFonts.getPhysicalFonts().get("liberation serif");
         if (font != null) {
           Mapper fontMapper = new IdentityPlusMapper();
           wordOutputDoc.setFontMapper(fontMapper, true);
