850423c86b114114a9f61147c3cd87218c39e2ed
haarli <haarlaender@mpdl.mpg.de>
2018-09-28 10:59:21 +0200

    Fixes for fileindex: Reset during reindex

--- pubman_logic/src/main/java/de/mpg/mpdl/inge/service/pubman/impl/PubItemServiceDbImpl.java
+++ pubman_logic/src/main/java/de/mpg/mpdl/inge/service/pubman/impl/PubItemServiceDbImpl.java
@@ -903,7 +903,6 @@ public class PubItemServiceDbImpl extends GenericServiceBaseImpl<ItemVersionVO> 
     logger.info("Reindexing object " + object.getObjectId());
 
     ItemVersionVO latestVersion = (ItemVersionVO) object.getLatestVersion();
-    latestVersion = latestVersion;
     // First try to delete the old version from index
     String oldVersion = new VersionableId(latestVersion.getObjectId(), latestVersion.getVersionNumber() - 1).toString();
     pubItemDao.delete(oldVersion);
@@ -923,7 +922,6 @@ public class PubItemServiceDbImpl extends GenericServiceBaseImpl<ItemVersionVO> 
 
     if (object.getLatestRelease() != null && object.getLatestRelease().getVersionNumber() != object.getLatestVersion().getVersionNumber()) {
       ItemVersionVO latestRelease = (ItemVersionVO) object.getLatestRelease();
-      latestRelease = latestRelease;
       logger.info("Reindexing item latest release " + latestRelease.getObjectIdAndVersion());
       if (immediate) {
         pubItemDao.createImmediately(latestRelease.getObjectId() + "_" + latestRelease.getVersionNumber(), latestRelease);
