71736bd8993cac38155eadfcf014ac1b9622c997
przMPDL <wurzelpuzz@users.noreply.github.com>
2019-02-28 15:40:42 +0100

    INGE-318: Import IV

--- aa_presentation/src/main/java/de/mpg/mpdl/inge/aa/web/AaStart.java
+++ aa_presentation/src/main/java/de/mpg/mpdl/inge/aa/web/AaStart.java
@@ -72,9 +72,6 @@ public class AaStart extends HttpServlet {
     String from = request.getParameter("from");
     String tan = request.getParameter("tan");
 
-    //    // TODO: Make this generic
-    //    String handle = request.getParameter("eSciDocUserHandle");
-
     Matcher matcher = authPattern.matcher(from);
     from = matcher.replaceAll("");
 
@@ -90,12 +87,6 @@ public class AaStart extends HttpServlet {
     }
     target += separator + "target=" + URLEncoder.encode(from, "ISO-8859-1") + "&tan=" + URLEncoder.encode(tan, "ISO-8859-1");
 
-    //    if (handle != null) {
-    //      target += "&eSciDocUserHandle=" + URLEncoder.encode(handle, "ISO-8859-1");
-    //    }
-
     response.sendRedirect(target);
-
   }
-
 }
