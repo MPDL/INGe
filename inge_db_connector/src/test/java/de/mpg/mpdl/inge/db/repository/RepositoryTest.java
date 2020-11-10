867608462d8a93247364056230828258be91bf04
przMPDL <wurzelpuzz@users.noreply.github.com>
2020-11-10 12:36:55 +0100

    inge_db_connector: new try for tests

--- inge_db_connector/src/test/java/de/mpg/mpdl/inge/db/repository/RepositoryTest.java
+++ inge_db_connector/src/test/java/de/mpg/mpdl/inge/db/repository/RepositoryTest.java
@@ -19,7 +19,7 @@ import de.mpg.mpdl.inge.model.db.valueobjects.ContextDbVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {JPAConfiguration.class})
-public class RepositoryTest_ {
+public class RepositoryTest {
 
   @Autowired
   AuditRepository auditRepository;
