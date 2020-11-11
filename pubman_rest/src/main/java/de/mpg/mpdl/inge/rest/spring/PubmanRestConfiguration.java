7ffb485084f372c084161450f7b1143a05a2a2c0
przMPDL <wurzelpuzz@users.noreply.github.com>
2020-11-11 08:35:17 +0100

    no cataline.home

--- pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/spring/PubmanRestConfiguration.java
+++ pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/spring/PubmanRestConfiguration.java
@@ -2,11 +2,8 @@ package de.mpg.mpdl.inge.rest.spring;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@PropertySources({@PropertySource(value = "file:${catalina.home}/conf/pubman.properties", ignoreResourceNotFound = true)})
 @ComponentScan(basePackages = {"de.mpg.mpdl.inge.rest"})
 public class PubmanRestConfiguration {
 }
