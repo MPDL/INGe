2554ec174ed82cc40a6aeb3fc12c67b30c03277d
wilhelmfrank <frank@mpdl.mpg.de>
2022-12-09 14:12:35 +0100

    streamline WebInitializer (pubman_rest)

--- pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/web/spring/WebInitializer.java
+++ pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/web/spring/WebInitializer.java
@@ -1,71 +1,17 @@
 package de.mpg.mpdl.inge.rest.web.spring;
 
-import javax.servlet.Filter;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
 import de.mpg.mpdl.inge.rest.spring.PubmanRestConfiguration;
-import de.mpg.mpdl.inge.rest.spring.WebConfiguration;
 import de.mpg.mpdl.inge.service.spring.AppConfigPubmanLogic;
 
 
-public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer implements WebApplicationInitializer {
-
-  @Override
-  protected Class<?>[] getRootConfigClasses() {
-    return new Class[] {PubmanRestConfiguration.class};
-  }
-
-  @Override
-  protected Class<?>[] getServletConfigClasses() {
-    return null;
-  }
-
-  @Override
-  protected String[] getServletMappings() {
-    return new String[] {"/"};
-  }
-
-  @Override
-  protected Filter[] getServletFilters() {
-    CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
-    encodingFilter.setEncoding("UTF-8");
-    encodingFilter.setForceEncoding(true);
-    return new Filter[] {encodingFilter, new AuthCookieToHeaderFilter()};
-  }
-
-  /*
-  @Override
-  public void onStartup(ServletContext servletContext) throws ServletException {
-    // Use ear.context (pubman_logic) as shared context between all webapps in the ear.
-    // ear.context is defined in beanRefContext.xml in module pubman_logic
-    servletContext.setInitParameter(ContextLoader.LOCATOR_FACTORY_KEY_PARAM, "ear.context");
-    super.onStartup(servletContext);
-  }
-  */
-
-  /*
-  @Override
-  public void onStartup(ServletContext servletContext) throws ServletException {
-  
-    var ctx = new AnnotationConfigWebApplicationContext();
-    ctx.register(WebConfiguration.class);
-    ctx.setServletContext(servletContext);
-  
-    var servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
-    servlet.setLoadOnStartup(1);
-    servlet.addMapping("/");
-  }
-  */
+public class WebInitializer implements WebApplicationInitializer {
 
   @Override
   public void onStartup(ServletContext container) {
