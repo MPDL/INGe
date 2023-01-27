d4a11a7f8272b58cd0c1bac9a3c644acbe2b1524
wilhelmfrank <frank@mpdl.mpg.de>
2023-01-27 16:13:00 +0100

    web+rest config

--- pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/spring/WebConfiguration.java
+++ pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/spring/WebConfiguration.java
@@ -1,9 +1,18 @@
 package de.mpg.mpdl.inge.rest.spring;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import de.mpg.mpdl.inge.model.util.MapperFactory;
 
 @Configuration
 @EnableWebMvc
@@ -17,6 +26,22 @@ public class WebConfiguration implements WebMvcConfigurer {
     viewResolver.setSuffix(".jsp");
     registry.viewResolver(viewResolver);
 
+  }
+
+  @Override
+  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    //First place: A Json converter using our default Jackson object mapper
+    MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+    converter.setObjectMapper(MapperFactory.getObjectMapper());
+    converters.add(0, converter);
+
+    //Second place: A String converter which allows to return strings as json
+    StringHttpMessageConverter smc = new StringHttpMessageConverter();
+    smc.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON_UTF8));
+    converters.add(0, smc);
+    //    System.out.println("Converters" + converters);
+
+    //WebMvcConfigurer.super.extendMessageConverters(converters);
   }
 
   @Override
