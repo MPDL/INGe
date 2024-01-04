6810a9f9ae35c4d58fae74cbd0f3b7102369167f
przMPDL <wurzelpuzz@users.noreply.github.com>
2024-01-04 14:49:49 +0100

    only formatting

--- pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/web/controller/OrganizationRestController.java
+++ pubman_rest/src/main/java/de/mpg/mpdl/inge/rest/web/controller/OrganizationRestController.java
@@ -324,12 +324,13 @@ public class OrganizationRestController {
   }
 
   @Hidden
-  @RequestMapping(value = "/elasticsearch", method = RequestMethod.POST, consumes = org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE,
-          produces = org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @RequestMapping(value = "/elasticsearch", method = RequestMethod.POST,
+      consumes = org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE,
+      produces = org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE)
   public ResponseEntity<String> searchDetailed(@RequestHeader(value = AuthCookieToHeaderFilter.AUTHZ_HEADER, required = false) String token,
-                                               @RequestBody JsonNode searchSource, @RequestParam(name = "scroll", required = false) String scrollTimeValue,
-                                               HttpServletResponse httpResponse)
-          throws AuthenticationException, AuthorizationException, IngeTechnicalException, IngeApplicationException, IOException {
+      @RequestBody JsonNode searchSource, @RequestParam(name = "scroll", required = false) String scrollTimeValue,
+      HttpServletResponse httpResponse)
+      throws AuthenticationException, AuthorizationException, IngeTechnicalException, IngeApplicationException, IOException {
 
     return UtilServiceBean.searchDetailed(organizationSvc, searchSource, scrollTimeValue, token, httpResponse);
   }
