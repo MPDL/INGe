a5c29caeaf8ec863811201cc576ffd899f4be0f6
przMPDL <wurzelpuzz@users.noreply.github.com>
2024-02-20 12:49:07 +0100

    Revert "not used anymore"
    This reverts commit e1eef72cb499ec193f6a853adb82b19363b8c111.

---
+++ model/src/main/java/de/mpg/mpdl/inge/model/valueobjects/MdsCollectionVO.java
@@ -0,0 +1,52 @@
+/*
+ * 
+ * CDDL HEADER START
+ * 
+ * The contents of this file are subject to the terms of the Common Development and Distribution
+ * License, Version 1.0 only (the "License"). You may not use this file except in compliance with
+ * the License.
+ * 
+ * You can obtain a copy of the license at license/ESCIDOC.LICENSE or
+ * http://www.escidoc.org/license. See the License for the specific language governing permissions
+ * and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL HEADER in each file and include the License
+ * file at license/ESCIDOC.LICENSE. If applicable, add the following below this CDDL HEADER, with
+ * the fields enclosed by brackets "[]" replaced with your own identifying information: Portions
+ * Copyright [yyyy] [name of copyright owner]
+ * 
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright 2006-2012 Fachinformationszentrum Karlsruhe Gesellschaft für
+ * wissenschaftlich-technische Information mbH and Max-Planck- Gesellschaft zur Förderung der
+ * Wissenschaft e.V. All rights reserved. Use is subject to license terms.
+ */
+
+package de.mpg.mpdl.inge.model.valueobjects;
+
+import de.mpg.mpdl.inge.model.valueobjects.metadata.PartnerVO;
+
+/**
+ * The set Collection describes resources which are Scholarly Workbench Collections which contain
+ * bjects and bundles.
+ * 
+ * @revised by MuJ: 28.08.2007
+ * @version $Revision$ $LastChangedDate$ by $Author$
+ * @updated 05-Sep-2007 10:30:49
+ */
+@SuppressWarnings("serial")
+public class MdsCollectionVO extends MetadataSetVO {
+  /**
+   * The person or organization who owns content of the collection.
+   */
+  private java.util.List<PartnerVO> partners = new java.util.ArrayList<PartnerVO>();
+
+  /**
+   * Delivers the list of partners of this collection.
+   */
+  public java.util.List<PartnerVO> getPartners() {
+    return partners;
+  }
+}
