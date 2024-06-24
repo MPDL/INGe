3a378e8fa31700e7e6ee3f5bc6bc1a8e7c9b7a8c
przMPDL <wurzelpuzz@users.noreply.github.com>
2024-06-24 11:06:59 +0200

    SitemapProvider II

--- pubman_logic/src/main/java/de/mpg/mpdl/inge/service/sitemap/SitemapProvider.java
+++ pubman_logic/src/main/java/de/mpg/mpdl/inge/service/sitemap/SitemapProvider.java
@@ -33,9 +33,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class SitemapProvider {
   private static final Logger logger = LogManager.getLogger(SitemapProvider.class);
 
