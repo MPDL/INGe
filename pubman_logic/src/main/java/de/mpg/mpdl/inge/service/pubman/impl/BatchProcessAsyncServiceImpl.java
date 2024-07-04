7556c918fb5870ab41bf58d8ca4f26f9bf2bea8c
przMPDL <wurzelpuzz@users.noreply.github.com>
2024-07-04 11:42:39 +0200

    temporary delay for async batch service V

--- pubman_logic/src/main/java/de/mpg/mpdl/inge/service/pubman/impl/BatchProcessAsyncServiceImpl.java
+++ pubman_logic/src/main/java/de/mpg/mpdl/inge/service/pubman/impl/BatchProcessAsyncServiceImpl.java
@@ -349,7 +349,7 @@ public class BatchProcessAsyncServiceImpl implements BatchProcessAsyncService, A
 
       // TODO remove
       try {
-        Thread.sleep(45000);
+        Thread.sleep(5000);
       } catch (InterruptedException e) {
         throw new RuntimeException(e);
       }
