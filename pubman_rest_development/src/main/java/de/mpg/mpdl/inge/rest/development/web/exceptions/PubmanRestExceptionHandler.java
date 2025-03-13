27ccaebec4ca37beb564fc37682f6134eea630d0
przMPDL <wurzelpuzz@users.noreply.github.com>
2025-03-13 13:56:50 +0100

    no more compiler erros

--- pubman_rest_development/src/main/java/de/mpg/mpdl/inge/rest/development/web/exceptions/PubmanRestExceptionHandler.java
+++ pubman_rest_development/src/main/java/de/mpg/mpdl/inge/rest/development/web/exceptions/PubmanRestExceptionHandler.java
@@ -1,25 +1,14 @@
 package de.mpg.mpdl.inge.rest.development.web.exceptions;
 
-import org.apache.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import org.springframework.beans.TypeMismatchException;
+import org.apache.logging.log4j.Logger;
 import org.springframework.hateoas.mediatype.vnderrors.VndErrors;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @ControllerAdvice
 public class PubmanRestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -53,38 +42,38 @@ public class PubmanRestExceptionHandler extends ResponseEntityExceptionHandler {
     return errors;
   }
 
-  @Override
-  protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status,
-      WebRequest request) {
-    Throwable mostSpecificCause = ex.getMostSpecificCause();
-    VndErrors.VndError errorMessage;
-    if (mostSpecificCause != null) {
-      String exceptionName = mostSpecificCause.getClass().getName();
-      String message = mostSpecificCause.getMessage();
-      errorMessage = new VndErrors.VndError(exceptionName, message);
-    } else {
-      errorMessage = new VndErrors.VndError("cause?", ex.getMessage());
-    }
-    return new ResponseEntity<Object>(errorMessage, headers, status);
-  }
-
-  @Override
-  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status,
-      WebRequest request) {
-    List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
-    List<ObjectError> globalErrors = ex.getBindingResult().getGlobalErrors();
-    List<VndErrors.VndError> errors = new ArrayList<VndErrors.VndError>(fieldErrors.size() + globalErrors.size());
-    VndErrors.VndError error;
-    for (FieldError fieldError : fieldErrors) {
-      error = new VndErrors.VndError("invalid!!!", fieldError.getField() + ", " + fieldError.getDefaultMessage());
-      errors.add(error);
-    }
-    for (ObjectError objectError : globalErrors) {
-      error = new VndErrors.VndError("invalid!!!", objectError.getObjectName() + ", " + objectError.getDefaultMessage());
-      errors.add(error);
-    }
-    VndErrors errorMessage = new VndErrors(errors, null, null, null);
-    return new ResponseEntity<Object>(errorMessage, headers, status);
-  }
+//  @Override
+//  protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status,
+//      WebRequest request) {
+//    Throwable mostSpecificCause = ex.getMostSpecificCause();
+//    VndErrors.VndError errorMessage;
+//    if (mostSpecificCause != null) {
+//      String exceptionName = mostSpecificCause.getClass().getName();
+//      String message = mostSpecificCause.getMessage();
+//      errorMessage = new VndErrors.VndError(exceptionName, message);
+//    } else {
+//      errorMessage = new VndErrors.VndError("cause?", ex.getMessage());
+//    }
+//    return new ResponseEntity<Object>(errorMessage, headers, status);
+//  }
+//
+//  @Override
+//  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status,
+//      WebRequest request) {
+//    List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+//    List<ObjectError> globalErrors = ex.getBindingResult().getGlobalErrors();
+//    List<VndErrors.VndError> errors = new ArrayList<VndErrors.VndError>(fieldErrors.size() + globalErrors.size());
+//    VndErrors.VndError error;
+//    for (FieldError fieldError : fieldErrors) {
+//      error = new VndErrors.VndError("invalid!!!", fieldError.getField() + ", " + fieldError.getDefaultMessage());
+//      errors.add(error);
+//    }
+//    for (ObjectError objectError : globalErrors) {
+//      error = new VndErrors.VndError("invalid!!!", objectError.getObjectName() + ", " + objectError.getDefaultMessage());
+//      errors.add(error);
+//    }
+//    VndErrors errorMessage = new VndErrors(errors, null, null, null);
+//    return new ResponseEntity<Object>(errorMessage, headers, status);
+//  }
 
 }
