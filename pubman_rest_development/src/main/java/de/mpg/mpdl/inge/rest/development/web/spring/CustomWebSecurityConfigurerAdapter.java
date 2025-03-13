7b60bc94c3ccb18caaa6769707a785cb307e3353
przMPDL <wurzelpuzz@users.noreply.github.com>
2025-03-13 15:42:42 +0100

    no more compiler erros

--- pubman_rest_development/src/main/java/de/mpg/mpdl/inge/rest/development/web/spring/CustomWebSecurityConfigurerAdapter.java
+++ pubman_rest_development/src/main/java/de/mpg/mpdl/inge/rest/development/web/spring/CustomWebSecurityConfigurerAdapter.java
@@ -1,31 +1,47 @@
 package de.mpg.mpdl.inge.rest.development.web.spring;
 
+import de.mpg.mpdl.inge.util.PropertyReader;
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import de.mpg.mpdl.inge.util.PropertyReader;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Custom WebSecurityConfigurerAdapter for development REST interface
- * 
+ *
  * @author walter
  *
  */
 @Configuration
 @EnableWebSecurity
-public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+public class CustomWebSecurityConfigurerAdapter {
 
-  @Override
-  public void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser(PropertyReader.getProperty(PropertyReader.INGE_REST_DEVELOPMENT_ADMIN_USERNAME))
-        .password("{noop}" + PropertyReader.getProperty(PropertyReader.INGE_REST_DEVELOPMENT_ADMIN_PASSWORD)).roles("ADMIN");
+  //  @Override
+  //  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+  //    auth.inMemoryAuthentication().withUser(PropertyReader.getProperty(PropertyReader.INGE_REST_DEVELOPMENT_ADMIN_USERNAME))
+  //        .password("{noop}" + PropertyReader.getProperty(PropertyReader.INGE_REST_DEVELOPMENT_ADMIN_PASSWORD)).roles("ADMIN");
+  //  }
+
+  //  @Override
+  //  protected void configure(HttpSecurity http) throws Exception {
+  //    http.authorizeRequests().antMatchers("/**").hasRole("ADMIN").and().httpBasic().and().csrf().disable();
+  //  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+    return new InMemoryUserDetailsManager(Arrays.asList(userBuilder.username(PropertyReader.getProperty(PropertyReader.INGE_REST_DEVELOPMENT_ADMIN_USERNAME))
+        .password("{noop}" + PropertyReader.getProperty(PropertyReader.INGE_REST_DEVELOPMENT_ADMIN_PASSWORD)).roles("ADMIN").build()));
   }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/**").hasRole("ADMIN").and().httpBasic().and().csrf().disable();
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.requestMatchers("/**").hasRole("ADMIN")).httpBasic();
+    return http.build();
   }
 }
