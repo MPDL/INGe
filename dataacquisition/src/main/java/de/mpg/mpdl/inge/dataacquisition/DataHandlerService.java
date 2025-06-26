b2196d81b49ce17d0b15b0e180622b52ca6c8d76
przMPDL <wurzelpuzz@users.noreply.github.com>
2025-06-26 17:14:38 +0200

    arXiv http -> https

--- dataacquisition/src/main/java/de/mpg/mpdl/inge/dataacquisition/DataHandlerService.java
+++ dataacquisition/src/main/java/de/mpg/mpdl/inge/dataacquisition/DataHandlerService.java
@@ -320,6 +320,7 @@ public class DataHandlerService {
 
     try {
       URLConnection con = url.openConnection();
+      logger.info("Source: " + con.getURL());
       HttpURLConnection httpCon = (HttpURLConnection) con;
 
       int responseCode = httpCon.getResponseCode();
