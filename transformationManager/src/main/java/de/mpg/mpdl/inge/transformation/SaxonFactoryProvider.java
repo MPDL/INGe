0a895182accf04c228961d0abd8b5108dc873b97
haarli <haarlaender@mpdl.mpg.de>
2025-11-21 17:32:42 +0100

    SaxonFactoryProvider as Singleton

--- transformationManager/src/main/java/de/mpg/mpdl/inge/transformation/SaxonFactoryProvider.java
+++ transformationManager/src/main/java/de/mpg/mpdl/inge/transformation/SaxonFactoryProvider.java
@@ -20,25 +20,49 @@ import net.sf.saxon.s9api.Processor;
 public final class SaxonFactoryProvider {
   private SaxonFactoryProvider() {}
 
+  private static volatile TransformerFactory CACHED_TF;
+
+  /**
+   * Returns a Saxon-backed TransformerFactory with all extension functions registered.
+   * Uses a lazily-initialized, thread-safe singleton to avoid repeated bootstrapping.
+   */
   public static TransformerFactory createWithExtensions() {
-    Processor proc = new Processor(false); // Saxon HE
+    TransformerFactory tf = CACHED_TF;
+    if (tf != null) {
+      return tf;
+    }
+    synchronized (SaxonFactoryProvider.class) {
+      if (CACHED_TF == null) {
+        Processor proc = new Processor(false); // Saxon HE
 
-    // Register citationmanager functions
-    proc.registerExtensionFunction(new IsCjkFunction());
-    proc.registerExtensionFunction(new EscapeMarkupTagsFunction());
-    proc.registerExtensionFunction(new GetCitationStyleForJournalFunction());
-    proc.registerExtensionFunction(new ConvertSnippetToHtmlFunction());
+        // Register citationmanager functions
+        proc.registerExtensionFunction(new IsCjkFunction());
+        proc.registerExtensionFunction(new EscapeMarkupTagsFunction());
+        proc.registerExtensionFunction(new GetCitationStyleForJournalFunction());
+        proc.registerExtensionFunction(new ConvertSnippetToHtmlFunction());
 
-    // Register transformationManager legacy java: wrappers to avoid editing XSLTs
-    proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.UtilQueryConeFunction(proc));
-    proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.UtilQueryConeExactFunction(proc));
-    proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.UtilGetMimetypeFunction());
-    proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.UtilGetSizeFunction(proc));
-    proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.UtilStripHtmlFunction());
-    proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.AuthorDecoderParseAsNodeFunction(proc));
-    proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.BibtexTexStringFunction());
+        // Register transformationManager functions
+        proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.UtilQueryConeFunction(proc));
+        proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.UtilQueryConeExactFunction(proc));
+        proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.UtilGetMimetypeFunction());
+        proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.UtilGetSizeFunction(proc));
+        proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.UtilStripHtmlFunction());
+        proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.AuthorDecoderParseAsNodeFunction(proc));
+        proc.registerExtensionFunction(new de.mpg.mpdl.inge.transformation.xslt.functions.BibtexTexStringFunction());
 
-    Configuration config = proc.getUnderlyingConfiguration();
-    return new TransformerFactoryImpl(config);
+        Configuration config = proc.getUnderlyingConfiguration();
+        CACHED_TF = new TransformerFactoryImpl(config);
+      }
+      return CACHED_TF;
+    }
+  }
+
+  /**
+   * For tests only: clears the cached factory so a fresh Processor/Configuration can be created.
+   */
+  static void resetForTests() {
+    synchronized (SaxonFactoryProvider.class) {
+      CACHED_TF = null;
+    }
   }
 }
