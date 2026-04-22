b9c313c6d4247fd35c6d4f579ecaf177e87dee3b
przMPDL <wurzelpuzz@users.noreply.github.com>
2026-04-22 14:10:46 +0200

    only formatting

--- inge_util/src/test/java/de/mpg/mpdl/inge/util/UrlValidatorTest.java
+++ inge_util/src/test/java/de/mpg/mpdl/inge/util/UrlValidatorTest.java
@@ -5,27 +5,27 @@ import static org.junit.Assert.*;
 
 public class UrlValidatorTest {
 
-    @Test
-    public void testValidateRelativeUrl() {
-        // This should not throw an exception if we want to support relative URLs like /cone/
-        UrlValidator.validate("/cone/");
-    }
+  @Test
+  public void testValidateRelativeUrl() {
+    // This should not throw an exception if we want to support relative URLs like /cone/
+    UrlValidator.validate("/cone/");
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testValidateInvalidUrl() {
-        UrlValidator.validate("http://malicious.com");
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void testValidateInvalidUrl() {
+    UrlValidator.validate("http://malicious.com");
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testValidateProtocolRelativeUrl() {
-        // Protocol-relative URLs (//example.com) should NOT be allowed as they are unsafe
-        UrlValidator.validate("//malicious.com");
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void testValidateProtocolRelativeUrl() {
+    // Protocol-relative URLs (//example.com) should NOT be allowed as they are unsafe
+    UrlValidator.validate("//malicious.com");
+  }
 
-    @Test
-    public void testValidateAbsoluteUrl() {
-        // Assuming localhost is always allowed or needs to be in properties.
-        // The current implementation allows localhost.
-        UrlValidator.validate("http://localhost:8080/cone/");
-    }
+  @Test
+  public void testValidateAbsoluteUrl() {
+    // Assuming localhost is always allowed or needs to be in properties.
+    // The current implementation allows localhost.
+    UrlValidator.validate("http://localhost:8080/cone/");
+  }
 }
